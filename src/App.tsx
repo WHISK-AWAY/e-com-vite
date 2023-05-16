682f241092c969c00573c7a35ffa2bb3ba5f3916
ruralFluffyDill <stacylukavsky@gmail.com>
2023-05-15 22:31:14 -0400

    add sign_in&&sign_up rts

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import {Route, Routes} from 'react-router-dom';
 import SignIn from "./components/SignIn";
-
+import SignUp from './components/SignUp';
 
 function App() {
   return (
-    <SignIn/>
+    <Routes>
+      
+
+    <Route path='/sign_in' element={<SignIn/>}/>
+    <Route path='/sign_up' element={<SignUp/>}/>
+    </Routes>
   );
 }
 
