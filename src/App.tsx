c81be861c09e924eacb4fba9709cc354843315e4
ruralFluffyDill <stacylukavsky@gmail.com>
2023-05-17 21:02:00 -0400

    add aditional routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import {Route, Routes} from 'react-router-dom';
 import SignIn from "./components/SignIn";
 import SignUp from './components/SignUp';
+import Homepage from'./components/Homepage'
+import AllProducts from './components/AllProducts';
 
 function App() {
   return (
     <Routes>
       
 
+    <Route path='/' element={<Homepage/>}/>
     <Route path='/sign_in' element={<SignIn/>}/>
     <Route path='/sign_up' element={<SignUp/>}/>
+    <Route path='/shop-all' element={<AllProducts/>}/>
     </Routes>
   );
 }
