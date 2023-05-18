47c27cdd49ad50cb2d2c2d5e46c42f207b7d5d87
ruralFluffyDill <stacylukavsky@gmail.com>
2023-05-17 20:48:39 -0400

    add user input field validation

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,51 +1,107 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useAppSelector, useAppDispatch } from '../redux/hooks';
 import { requestLogin, selectAuth } from '../redux/slices/authSlice';
 import { Link } from 'react-router-dom';
-
+import { z, ZodType } from 'zod';
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
+import { zodResolver } from '@hookform/resolvers/zod';
 const VITE_API_URL = import.meta.env.VITE_API_URL;
 
+export type FormData = {
+  email: string;
+  password: string;
+};
+
+const zodLogin: ZodType<FormData> = z
+  .object({
+    email: z.string().email(),
+    password: z.string().min(8).max(20),
+  })
+  .strict();
+
 export default function SignIn() {
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
   const dispatch = useAppDispatch();
-  // const loginSelector = useAppSelector(selectAuth);
+  const selectAuthUser = useAppSelector(selectAuth);
 
-  // console.log('LS', loginSelector);
-  const handleClick = async (e: any) => {
-    e.preventDefault();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    setError,
+    clearErrors,
+    formState: { errors, dirtyFields },
+  } = useForm<FormData>({
+    resolver: zodResolver(zodLogin),
+    defaultValues: { email: '', password: '' },
+  });
 
-    dispatch(requestLogin({ email, password }));
+  const emailFetcher = async (email: any) => {
+    try {
+      const { data } = await axios.post(
+        VITE_API_URL + '/api/auth/check-email',
+        {
+          email,
+        }
+      );
+      if (!data.message) {
+        reset({
+          email: '',
+        });
+        setError('email', {
+          type: 'custom',
+          message: 'Email does not exist',
+        });
+      } else {
+
+        clearErrors('email')
+      }
+
+    } catch (err) {
+      console.log(err);
+    }
   };
+
+  useEffect(() => {
+    if (selectAuthUser.error.status === 403) {
+      reset({
+        password: '',
+      });
+      setError('password', {
+        type: 'custom',
+        message: 'Incorrect password',
+      });
+    } else {
+      clearErrors('password');
+    }
+  }, [selectAuthUser]);
+
+  const submitData = (data: FormData) => {
+    dispatch(requestLogin(data));
+  };
+
 
   return (
     <section className='form-container'>
       <div>
         <h1>SIGN IN</h1>
-        <form className='sign-in-form'>
+        <form className='sign-in-form' onSubmit={handleSubmit(submitData)}>
           <div className='email-field'>
             <label htmlFor='email'>email</label>
             <input
               type='email'
-              name='email'
-              id='email'
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              {...register('email', {
+                onBlur: (e) => emailFetcher(e.target.value),
+              })}
             />
+            {errors.email && <p>{errors.email.message}</p>}
           </div>
           <div className='password-field'>
             <label htmlFor='password'>password</label>
-            <input
-              type='password'
-              name='password'
-              id='password'
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
+            <input type='password' {...register('password')} />
+            {errors.password && <p>{errors.password.message}</p>}
           </div>
-          <button type='submit' onClick={async (e) => await handleClick(e)}>
-            LOG IN
-          </button>
+          <input type='submit' />
         </form>
         <p>
           don't have an account? sign up <Link to={'/sign_up'}>here</Link>
