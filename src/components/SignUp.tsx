e34e87afb8534fe36e56f5060c46bcb24e0d4b84
ruralFluffyDill <stacylukavsky@gmail.com>
2023-05-17 20:48:57 -0400

    add user input field validation+ signin logic

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,80 +1,165 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { selectAuth, requestSignUp } from '../redux/slices/authSlice';
 import { useAppDispatch, useAppSelector } from '../redux/hooks';
+import axios from 'axios';
+import { ZodType, z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+const VITE_API_URL = import.meta.env.VITE_API_URL;
+
 
 export default function SignUp() {
-  const [firstName, setFirstName] = useState<string>('');
-  const [lastName, setLastName] = useState<string>('');
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
-  const [confirmPassword, setConfirmPassword] = useState<string>('');
+  const zodUser: ZodType<FormData> = z
+    .object({
+      firstName: z.string().min(1),
+      lastName: z.string().min(1),
+      email: z.string().email(),
+      password: z.string().min(8).max(20),
+      address: z
+        .object({
+          address_1: z.string(),
+          address_2: z.string().optional(),
+          city: z.string(),
+          state: z.string(),
+          zip: z.string(),
+        })
+        .optional(),
+      confirmPassword: z.string().min(8).max(20),
+    })
+    .strict()
+    .superRefine(({ confirmPassword, password }, ctx) => {
+      if (password !== confirmPassword) {
+        ctx.addIssue({
+          code: 'custom',
+          message: 'Password fields do  not match',
+          path: ['confirmPassword'],
+        });
+      }
+    });
+
+  type FormData = {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+  };
+
 
   const dispatch = useAppDispatch();
+  const selectAuthUser = useAppSelector(selectAuth);
 
 
-  const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    e.preventDefault();
-    dispatch(requestSignUp({
-      firstName, 
-      lastName, 
-      email, 
-      password,
-      confirmPassword
-    }));
+  
+  console.log('selector', selectAuthUser);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    setError,
+    clearErrors, 
+    formState: { errors, dirtyFields },
+  } = useForm<FormData>({
+    resolver: zodResolver(zodUser),
+    defaultValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    },
+  });
+  
 
+    const emailFetcher = async (email: any) => {
+      try {
+        const { data } = await axios.post(
+          VITE_API_URL + '/api/auth/check-email',
+          {
+            email,
+          }
+        );
+        if (data.message) {
+          reset({
+            email: '',
+          });
+          setError('email', { type: 'custom', message: 'Email already exists' });
+        } else {
+          clearErrors('email')
+        }
+        // console.log('userEmail', data)
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+
+    console.log('errors', errors);
     
+    useEffect(() => {
+      // console.log('err.email', errors.email);
+      // if (selectAuthUser.error.status === 409) {
+        //   reset({
+          //     email: '',
+    //   });
+    //   setError('email', { type: 'custom', message: 'Email already exists' });
+    // }
+    
+    if (errors.confirmPassword) {
+      reset(
+        {
+          password: '',
+          confirmPassword: '',
+        },
+        {
+          keepErrors: true,
+        }
+        );
+      }
+      emailFetcher(dirtyFields.email)
+
+  }, [ errors.confirmPassword]);
+
+  const submitData = (data: FormData) => {
+    // e.preventDefault();
+    console.log('works', data);
+    dispatch(requestSignUp(data));
   };
+
   return (
     <section className='sign-up-form-container'>
       <div>
         <h1>SIGN UP</h1>
-        <form className='sign-up-form'>
+        <form className='sign-up-form' onSubmit={handleSubmit(submitData)}>
           <div className='first-name-field'>
             <label htmlFor='first-name'>first name</label>
-            <input
-              type='text'
-              name='first-name'
-              id='first-name'
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
-            ></input>
+            <input type='text' {...register('firstName')}></input>
+            {errors.firstName && <p>{errors.firstName.message}</p>}
           </div>
           <div className='last-name-field'>
             <label htmlFor='last-name'>last name</label>
-            <input
-              type='text'
-              name='last-name'
-              id='last-name'
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
-            ></input>
+            <input type='text' {...register('lastName')}></input>
+            {errors.lastName && <p>{errors.lastName.message}</p>}
           </div>
           <div className='email-field'>
             <label htmlFor='email'>email</label>
-            <input
-              type='email'
-              name='email'
-              id='email'
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            ></input>
+            <input type='email' {...register('email', { onBlur:(e) => emailFetcher(e.target.value) })}></input>
+            {errors.email && <p>{errors.email.message}</p>}
           </div>
           <div className='password-field'>
             <label htmlFor='password'>password</label>
-            <input
-              type='password'
-              name='password'
-              id='password'
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            ></input>
+            <input type='password' {...register('password')}></input>
+            {errors.password && <p>{errors.password.message}</p>}
           </div>
           <div className='confirm-password-field'>
             <label htmlFor='confirm-password'>confirm password</label>
-            <input type='password' name='confirm-password' id='confirm-password' value={confirmPassword} onChange={e=> setConfirmPassword(e.target.value)}></input>
+            <input type='password' {...register('confirmPassword')}></input>
+            {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
           </div>
-          
-          <button onClick={handleClick}>SIGN UP</button>
+
+          <input type='submit' />
+          {/* <button>submit</button> */}
+          {/* <button type='submit'>submit</button> */}
         </form>
       </div>
     </section>
