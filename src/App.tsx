443ff6e1769f7c49c0e93a79a9d69a30ad6cede2
ruralFluffyDill <stacylukavsky@gmail.com>
2023-05-18 23:33:43 -0400

    add rts

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 import Homepage from './components/Homepage';
 import AllProducts from './components/AllProducts';
+import SingleProduct from './components/SingleProduct';
+import UserProfile from './components/UserProfile';
+import Cart from './components/Cart';
 
 function App() {
   return (
@@ -11,6 +14,10 @@ function App() {
       <Route path="/sign-in" element={<SignIn />} />
       <Route path="/sign-up" element={<SignUp />} />
       <Route path="/shop-all" element={<AllProducts />} />
+      <Route path="/product/:productId" element={<SingleProduct />} />
+      <Route path="/user/:userId" element={<UserProfile/>} />
+      <Route path="/user/:userId/cart" element={<Cart/>} />
+
     </Routes>
   );
 }
