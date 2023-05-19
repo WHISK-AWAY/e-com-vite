61b4be497c2af4a3605d1220ba6e817efd4c2efc
Patrick Bryan <pbryan9@gmail.com>
2023-05-19 09:57:00 -0700

    copy whole user type into express.user custom

--- server/custom.d.ts
+++ server/custom.d.ts
@@ -1,3 +1,5 @@
+// import { IUser } from './database/dbTypes';
+
 declare namespace Express {
   export interface Request {
     isAuthenticated: () => boolean;
@@ -7,5 +9,22 @@ declare namespace Express {
   export interface User {
     _id: string;
     firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    address?: {
+      address_1: string;
+      address_2?: string;
+      city: string;
+      state: string;
+      zip: string;
+    };
+    favorites?: mongoose.Types.ObjectId[];
+    cart: ICart;
+    role: 'admin' | 'user' | 'guest';
+    reviewCount?: number;
+    voteCount?: number;
+    skinConcerns?: string[];
+    purgeInactiveCart?(): void;
   }
 }
