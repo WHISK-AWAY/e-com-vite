c1ab96ebf5f75fc641aed233f9b9c7a7d827487e
Patrick Bryan <pbryan9@gmail.com>
2023-05-20 07:48:00 -0700

    correct ?page=0 issue on mount

--- src/components/AllProducts.tsx
+++ src/components/AllProducts.tsx
@@ -1,59 +1,64 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../redux/hooks';
 import {
   fetchAllProducts,
   selectAllProducts,
 } from '../redux/slices/allProductSlice';
-
 import { useSearchParams, Link } from 'react-router-dom';
+
+const PRODS_PER_PAGE = 9;
 
 export default function AllProducts() {
   const dispatch = useAppDispatch();
 
+  const [params, setParams] = useSearchParams();
+  const [pageNum, setPageNum] = useState<number | undefined>();
+  let curPage = Number(params.get('page'));
   const allProducts = useAppSelector(selectAllProducts);
 
-  let [page, setPage] = useSearchParams();
+  const maxPages = Math.ceil(allProducts.count! / PRODS_PER_PAGE);
+
+  useEffect(() => {
+    if (!curPage) setParams({ page: '1' });
+    else setPageNum(Number(params.get('page')));
+  }, [curPage]);
+
+  useEffect(() => {
+    if (pageNum && pageNum > 0) dispatch(fetchAllProducts(pageNum));
+  }, [pageNum]);
 
   const pageIncrementor = () => {
-    const pageNum = Number(page.get('page')) + 1;
-    if(pageNum > maxPages) return;
-    setPage({ page: String(pageNum) });
+    const nextPage = curPage + 1;
+    if (nextPage > maxPages) return;
+    setParams({ page: String(nextPage) });
   };
 
   const pageDecrementor = () => {
-    const pageNum = Number(page.get('page')) -1;
-    if(pageNum < 1) return;
-    setPage({page: String(pageNum)});
-  }
-  const prodsPerPage = 9;
-  const maxPages = Math.ceil(allProducts.count! / prodsPerPage);
-  
-  useEffect(() => {
-    if(!page.get('page')) {
-      setPage({page: String(1)})
-    }
-  }, [])
-
-  
-  useEffect(() => {
-    dispatch(fetchAllProducts(Number(page.get('page'))));
-  }, [page]);
+    const prevPage = curPage - 1;
+    if (prevPage < 1) return;
+    setParams({ page: String(prevPage) });
+  };
 
   if (!allProducts.products.length) return <p>...Loading</p>;
   return (
-    <section className='all-product-container'>
-      <h1 className='text-2xl'>SHOP ALL</h1>
+    <section className="all-product-container">
+      <h1 className="text-2xl">SHOP ALL</h1>
       <div>
         {allProducts.products.map((product) => (
-          <li className='list-none' key={product._id.toString()}>
-            <img src={product.imageURL} alt='cat' />
-            <p><Link to={'/product/'+ product._id}> {product.productName.toUpperCase()} </Link></p>
+          <li className="list-none" key={product._id.toString()}>
+            <img src={product.imageURL} alt="cat" />
+            <p>
+              <Link to={'/product/' + product._id}>
+                {' '}
+                {product.productName.toUpperCase()}{' '}
+              </Link>
+            </p>
             <p>{product.productShortDesc}</p>
             <p> {product.price}</p>
           </li>
         ))}
         <button onClick={pageIncrementor}>next</button>
-        <br/>
+        <br />
         <button onClick={pageDecrementor}>previous</button>
       </div>
     </section>
