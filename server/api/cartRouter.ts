52b25e99de074498e8ea68e0bf3a3d8475a4be62
ruralFluffyDill <stacylukavsky@gmail.com>
2023-05-20 15:00:00 -0400

    return updatedcart from backend

--- server/api/cartRouter.ts
+++ server/api/cartRouter.ts
@@ -17,7 +17,6 @@ router.get('/', checkAuthenticated, sameUserOrAdmin, async (req, res, next) => {
     // console.log('ID', userId);
     const user = await User.findById(userId, 'cart').populate({
       path: 'cart.products.product',
-      populate: { path: 'tags' },
     });
     if (user === null) return res.status(404).send('User does not exist');
 
@@ -39,17 +38,16 @@ router.post(
       const { productId, qty } = zodProduct.parse(req.body);
 
       const addItem = await user.cart.addProduct!(productId, qty);
+      const updatedCart = await User.findById(userId, 'cart').populate({
+        path: 'cart.products.product',
+      });;
 
-      
       if (!addItem)
         res
           .status(409)
           .json({ message: 'Not enough inventory - nothing added to cart' });
 
-     
-
-
-      res.status(201).json(addItem);
+      res.status(200).json(updatedCart);
     } catch (err) {
       next(err);
     }
@@ -57,7 +55,7 @@ router.post(
 );
 
 router.post(
-  '/removeItem',
+  '/remove-item',
   checkAuthenticated,
   sameUserOrAdmin,
   async (req, res, next) => {
@@ -69,18 +67,20 @@ router.post(
 
       const { productId, qty } = zodProduct.parse(req.body);
 
-      const removeItem = await user.cart.removeProduct!!!(productId, qty);
-
       const minQtyCheck = await User.findById(userId);
       const insufficientQty = minQtyCheck?.cart.products.filter(
         (qty) => qty.qty < 1
       );
 
-      console.log('IQ', insufficientQty);
       if (insufficientQty?.length! > 0)
         return res.status(500).send('Insuffient quantity');
 
-      res.status(204).json(removeItem);
+      const removeItem = await user.cart.removeProduct!!!(productId, qty);
+      const updatedCart = await User.findById(userId, 'cart').populate({
+        path: 'cart.products.product',
+      });
+
+      res.status(200).json(updatedCart);
     } catch (err) {
       next(err);
     }
