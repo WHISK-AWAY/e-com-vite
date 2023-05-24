deac960df80e271741ce51a727cb16e17169e250
Patrick Bryan <pbryan9@gmail.com>
2023-05-24 08:52:15 -0700

    remove address2 length req

--- src/components/UserAccount/EditShippingInfo.tsx
+++ src/components/UserAccount/EditShippingInfo.tsx
@@ -1,6 +1,6 @@
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import { TUser } from '../../redux/slices/userSlice';
-import { useForm } from 'react-hook-form';
+import { appendErrors, useForm } from 'react-hook-form';
 import { editUserAccountInfo } from '../../redux/slices/userSlice';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -19,7 +19,7 @@ type ShippingInfoFields = {
 
 const ZShippingData = z.object({
   address_1: z.string().min(5),
-  address_2: z.string().min(2).optional(),
+  address_2: z.string().optional(),
   city: z.string().min(1),
   state: z.string().min(2),
   zip: z.string().min(5),
@@ -47,7 +47,7 @@ export default function ShippingInfo({ user }: ShippingProps) {
     setError,
     getValues,
     setValue,
-    formState: { errors, dirtyFields },
+    formState: { errors },
   } = useForm<ShippingInfoFields>({
     resolver: zodResolver(ZShippingData),
     defaultValues,
@@ -57,40 +57,42 @@ export default function ShippingInfo({ user }: ShippingProps) {
   if (!address) return <h1>No addresses saved...</h1>;
 
   const submitData = (data: ShippingInfoFields) => {
+    console.log('inside submitData');
     dispatch(
       editUserAccountInfo({ userId: user._id!, user: { address: data } })
     );
   };
 
   return (
-    <div className='edit-shipping-info-container'>
+    <div className="edit-shipping-info-container">
       <h1>SHIPPING INFO</h1>
       <form onSubmit={handleSubmit(submitData)}>
-        <div className='address-1-field'>
-          <label htmlFor='address_1'>Address_1</label>
-          <input id='address_1' type='text' {...register('address_1')} />
+        <div className="address-1-field">
+          <label htmlFor="address_1">Address_1</label>
+          <input id="address_1" type="text" {...register('address_1')} />
         </div>
 
-        <div className='address-2-field'>
-          <label htmlFor='address_2'>Address_2</label>
-          <input id='address_2' type='text' {...register('address_2')} />
+        <div className="address-2-field">
+          <label htmlFor="address_2">Address_2</label>
+          <input id="address_2" type="text" {...register('address_2')} />
+          {errors.address_2 && <p>{errors.address_2.message}</p>}
         </div>
 
-        <div className='city-field'>
-          <label htmlFor='city'>City</label>
-          <input id='city' type='text' {...register('city')} />
+        <div className="city-field">
+          <label htmlFor="city">City</label>
+          <input id="city" type="text" {...register('city')} />
         </div>
 
-        <div className='state-field'>
-          <label htmlFor='state'>State</label>
-          <input id='state' type='text' {...register('state')} />
+        <div className="state-field">
+          <label htmlFor="state">State</label>
+          <input id="state" type="text" {...register('state')} />
         </div>
 
-        <div className='zip-field'>
-          <label htmlFor='zip'>Zip</label>
-          <input id='zip' type='text' {...register('zip')} />
+        <div className="zip-field">
+          <label htmlFor="zip">Zip</label>
+          <input id="zip" type="text" {...register('zip')} />
         </div>
-        <button type='submit'>SAVE</button>
+        <button type="submit">SAVE</button>
       </form>
     </div>
   );
