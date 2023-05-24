6dbf65749457235d52508874a9e8a7afd252d892
ruralFluffyDill <stacylukavsky@gmail.com>
2023-05-24 19:50:37 -0400

    add favorite item component

---
+++ src/components/FavoriteItem.tsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import { TProduct } from "../redux/slices/userSlice";
+
+export type TFavoriteItemProp = {
+  product: TProduct,
+  userId: string,
+  handleAddToCart: ({userId, productId, qty}: {
+    userId: string,
+    productId: string,
+    qty:number
+  }) => void
+}
+
+export default function FavoriteItem({product, userId,  handleAddToCart}:TFavoriteItemProp) {
+    
+  const [count, setCount] = useState<number>(1);
+
+      const totalQty = product.qty;
+      const userQty = count;
+
+      const qtyDecrementer = () => {
+        if (userQty <= 1) return count;
+        else setCount(userQty - 1);
+      };
+
+      const qtyIncrementor = () => {
+        if (userQty >= totalQty) return totalQty;
+        else setCount(userQty + 1);
+      };
+
+   return (
+     <section>
+       <div>
+         <h2>{product.productName}</h2>
+         <img src={product.imageURL} />
+         <p>{product.brand}</p>
+
+         <p>{product.productShortDesc}</p>
+         <p>{product.price}</p>
+         <button onClick={qtyDecrementer}>-</button>
+         <p>{count}</p>
+         <button onClick={qtyIncrementor}>+</button>
+         <button
+           onClick={() =>
+             handleAddToCart({
+               userId,
+               productId: product._id.toString(),
+               qty: count,
+             })
+           }
+         >
+           add to cart
+         </button>
+       </div>
+       <br />
+     </section>
+   );
+}
