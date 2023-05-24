c33821c82e98ce704f03430c11382a6f923a97b1
ruralFluffyDill <stacylukavsky@gmail.com>
2023-05-24 19:50:17 -0400

    add favorite component logc

---
+++ src/components/Favorite.tsx
@@ -0,0 +1,80 @@
+import {
+  fetchSingleUser,
+  selectSingleUserFavorites,
+  removeFromFavorites,
+} from '../redux/slices/userSlice';
+import { useAppDispatch, useAppSelector } from '../redux/hooks';
+import { useEffect, useState } from 'react';
+import { getUserId, selectAuth } from '../redux/slices/authSlice';
+import { addToCart } from '../redux/slices/cartSlice';
+import FavoriteItem from './FavoriteItem';
+
+export default function Favorite() {
+  const dispatch = useAppDispatch();
+  const userFavorite = useAppSelector(selectSingleUserFavorites);
+  const { userId } = useAppSelector(selectAuth);
+
+  useEffect(() => {
+    if (userId) dispatch(fetchSingleUser(userId));
+  }, [userId]);
+
+  const handleRemove = ({
+    userId,
+    productId,
+  }: {
+    userId: string;
+    productId: string;
+  }) => {
+    dispatch(removeFromFavorites({ userId, productId }));
+  };
+
+  const handleAddToCart = ({
+    userId,
+    productId,
+    qty,
+  }: {
+    userId: string;
+    productId: string;
+    qty: number;
+  }) => {
+    if (userId)
+      dispatch(addToCart({ userId, productId, qty })).then(() =>
+        handleRemove({ userId, productId })
+      );
+  };
+
+  if (!userFavorite || !userFavorite[0]) return <p>...Loading</p>;
+
+  if (!userId) return <p>...Loading</p>;
+  return (
+    <section className='user-favorites-container'>
+      <div>
+        <h1>FAVORITES</h1>
+        <div className='favorite-product-info'>
+          {userFavorite.map((product, productId) => {
+            return (
+              <div key={product._id}>
+                <FavoriteItem
+                  product={product}
+                  handleAddToCart={handleAddToCart}
+                  userId={userId}
+                />
+                <div>
+                  <button
+                    onClick={() =>
+                      handleRemove({ userId, productId: product._id })
+                    }
+                  >
+                    remove
+                  </button>
+                  <br />
+                </div>
+              </div>
+            );
+          })}
+          ;
+        </div>
+      </div>
+    </section>
+  );
+}
