03375a6f8e579c0048cc3f323265cffa353b17e3
ruralFluffyDill <stacylukavsky@gmail.com>
2023-05-31 23:03:56 -0400

    add checkout component

--- src/App.tsx
+++ src/App.tsx
@@ -8,31 +8,32 @@ import SingleProduct from './components/SingleProduct';
 import UserProfile from './components/UserAccount/UserProfile';
 import Cart from './components/Cart';
 import Navbar from './components/Navbar';
-import Checkout from './components/Checkout';
+import Checkout from './components/CheckoutProcess/Checkout';
 import Favorite from './components/Favorite';
-import Success from './components/UserAccount/stripe/Success';
-import Failure from './components/UserAccount/stripe/Failure';
+import Success from './components/CheckoutProcess/stripe/Success';
+import Failure from './components/CheckoutProcess/stripe/Failure';
+import Recap from './components/CheckoutProcess/Recap';
 
 function App() {
   return (
     <React.Fragment>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/shop-all" element={<AllProducts />} />
+        <Route path='/' element={<Homepage />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/shop-all' element={<AllProducts />} />
         <Route
-          path="/shop-all/bestsellers"
-          element={<AllProducts sortKey="saleCount" />}
+          path='/shop-all/bestsellers'
+          element={<AllProducts sortKey='saleCount' />}
         />
-        <Route path="/product/:productId" element={<SingleProduct />} />
-        <Route path="/user/:userId" element={<UserProfile />} />
-        <Route path="/user/:userId/cart" element={<Cart />} />
-        <Route path="/user/:userId/favorites" element={<Favorite />} />
-        {/* <Route path="/checkout" element={<Checkout />} /> */}
-        <Route path="/checkout/success" element={<Success />} />
-        <Route path="/checkout/failure" element={<Failure />} />
+        <Route path='/product/:productId' element={<SingleProduct />} />
+        <Route path='/user/:userId' element={<UserProfile />} />
+        <Route path='/user/:userId/cart' element={<Cart />} />
+        <Route path='/user/:userId/favorites' element={<Favorite />} />
+        <Route path="/checkout" element={<Recap />} />
+        <Route path='/checkout/success' element={<Success />} />
+        <Route path='/checkout/failure' element={<Failure />} />
       </Routes>
     </React.Fragment>
   );
