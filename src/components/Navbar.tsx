ab6ecd739eaa7ae59670221002defab4f07602b1
Patrick Bryan <pbryan9@gmail.com>
2023-06-01 10:15:18 -0700

    reset user state upon logout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -49,7 +49,7 @@ export default function Navbar() {
 
   function signOut() {
     dispatch(requestLogout());
-    // dispatch(resetUserState())
+    dispatch(resetUserState())
     navigate('/');
   }
 
