aa060be15fca7b54d9aeb4a598236ad93593eb9d
ruralFluffyDill <stacylukavsky@gmail.com>
2023-06-01 22:45:09 -0400

    add create order logic

--- src/components/CheckoutProcess/Recap.tsx
+++ src/components/CheckoutProcess/Recap.tsx
@@ -18,6 +18,12 @@ import axios from 'axios';
 import { StripeElementsOptions, loadStripe } from '@stripe/stripe-js';
 import Checkout from './Checkout';
 import { Elements } from '@stripe/react-stripe-js';
+import { TOrder, createOrder } from '../../redux/slices/orderSlice';
+import {
+  fetchSinglePromo,
+  selectPromo,
+  selectPromoErrors,
+} from '../../redux/slices/promoCodeSlice';
 // import { checkAddress } from '../../utilities/googleAddressValidation';
 
 type ShippingInfoFields = {
@@ -35,8 +41,12 @@ export default function Recap() {
   const dispatch = useAppDispatch();
   const userId = useAppSelector(selectAuthUserId);
   const { user } = useAppSelector(selectSingleUser);
+  const verifyPromo = useAppSelector(selectPromo);
+  const promoErrors = useAppSelector(selectPromoErrors);
   const [addressValidationFailed, setAddressValidationFailed] = useState(false);
   const [saveIsDisabled, setSaveIsDisabled] = useState(true);
+  const [promo, setPromo] = useState<string>('');
+  const [isCheckoutCancel, setIsCheckoutCancel] = useState<boolean>(false);
   const [clientSecret, setClientSecret] = useState<string>('');
   const [isFormEdit, setIsFormEdit] = useState<boolean>(false);
   const VITE_STRIPE_PUBLIC_KEY = import.meta.env.VITE_STRIPE_PUBLIC_KEY;
@@ -45,6 +55,76 @@ export default function Recap() {
   useEffect(() => {
     if (userId) dispatch(fetchSingleUser(userId));
   }, [userId]);
+
+  /**
+   * *ORDER CREATION WITH PENDING STATUS
+   */
+
+  const handlePromoSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (promo) dispatch(fetchSinglePromo(promo));
+  };
+
+  // useEffect(() => {
+  //   if (promoName) dispatch(fetchSinglePromo(promoName));
+  // }, [promoName]);
+
+  const orderDetails = () => {
+    console.log('hello from orderDetails function @ Success.tsx');
+    const userOrder = {} as Partial<TOrder>;
+    userOrder.orderDetails = [];
+    for (let product of user.cart.products) {
+      userOrder.orderDetails.push({
+        productId: product._id,
+        productName: product.product.productName,
+        productLongDesc: product.product.productLongDesc,
+        productShortDesc: product.product.productShortDesc,
+        brand: product.product.brand,
+        imageURL: product.product.imageURL,
+        price: product.price,
+        qty: product.qty,
+      });
+    }
+
+    const usr = {
+      user: {
+        userId: userId,
+        shippingInfo: {
+          firstName: user.firstName,
+          lastName: user.lastName,
+          email: user.email,
+          address_1: user.address!.address_1,
+          address_2: user.address?.address_2,
+          city: user.address?.city!,
+          state: user.address?.state!,
+          zip: user.address?.zip!,
+        },
+      },
+    } as Pick<TOrder, 'user'>;
+
+    userOrder.orderStatus = 'pending';
+
+    if (verifyPromo.promoCodeName) {
+      userOrder.promoCode = {
+        promoCodeName: promo,
+        promoCodeRate: verifyPromo.promoRate,
+      };
+    }
+
+    userOrder.date = new Date();
+    userOrder.user = usr.user;
+
+    console.log('UO', userOrder);
+    return userOrder;
+  };
+
+  // useEffect(() => {
+  //   if (user?._id && userId) {
+  //     dispatch(createOrder({ userId, order: orderDetails() as TOrder }));
+  //   }
+  // }, [user, userId]);
+
+  //address shit
 
   useEffect(() => {
     if (user && !user.address?.address_1) {
@@ -90,20 +170,28 @@ export default function Recap() {
 
   const handleCheckout = async (e: any) => {
     e.preventDefault();
-    // console.log('hi');
     try {
-      const { data } = await axios.post(
-        'http://localhost:3001/api/checkout/create-payment-intent',
-        {},
-        { withCredentials: true }
-      );
-      setClientSecret(data.clientSecret);
-      // console.log('data', data);
-      return data;
+      if (userId) {
+        await dispatch(
+          createOrder({ userId, order: orderDetails() as TOrder })
+        );
+        const { data } = await axios.post(
+          'http://localhost:3001/api/checkout/create-payment-intent',
+          {},
+          { withCredentials: true }
+        );
+        setClientSecret(data.clientSecret);
+
+        return data;
+      }
     } catch (err) {
       console.log(err);
     }
   };
+
+  useEffect(() => {
+    if (promoErrors.status) setPromo('');
+  }, [promoErrors.status]);
 
   const options: StripeElementsOptions = {
     clientSecret: clientSecret!,
@@ -117,13 +205,11 @@ export default function Recap() {
         fontFamily: 'Ideal Sans, system-ui, sans-serif',
         spacingUnit: '2px',
         borderRadius: '4px',
-        // See all possible variables below
       },
     },
   };
 
   const handleEditForm = (data: ShippingInfoFields) => {
-    // e.preventDefault();
     const userFields = {
       firstName: data.firstName,
       lastName: data.lastName,
@@ -137,14 +223,20 @@ export default function Recap() {
       },
     };
     if (userId) dispatch(editUserAccountInfo({ userId, user: userFields }));
+    
     setIsFormEdit(false);
   };
 
   // ! Decline card: 4000 0000 0000 9995
 
+  const handleCancel = () => {
+    setClientSecret('');
+    setIsCheckoutCancel(true);
+  };
+
   return (
     <div>
-      <section className="order-recap">
+      <section className='order-recap'>
         {/* PRODUCTS RECAP */}
 
         <h1>ORDER RECAP</h1>
@@ -157,11 +249,17 @@ export default function Recap() {
             </div>
           );
         })}
-        <h2>Subtotal: {user.cart.subtotal}</h2>
 
+        <h2>Subtotal: {user.cart.subtotal}</h2>
+        {verifyPromo.promoRate && (
+          <>
+            <p>discount: -{user.cart.subtotal * verifyPromo.promoRate}</p>
+            <p>total: {user.cart.subtotal * (1 - verifyPromo.promoRate)}</p>
+          </>
+        )}
         {/* SHIPPING FORM (PLAIN TEXT) */}
         {user && user.address && !isFormEdit && (
-          <section className="shipping-info-container">
+          <section className='shipping-info-container'>
             <br />
             <h1>SHIPPING INFO</h1>
             <div>
@@ -178,8 +276,8 @@ export default function Recap() {
             </div>
           </section>
         )}
-        {!isFormEdit && (
-          <button className="bg-blue-800" onClick={() => setIsFormEdit(true)}>
+        {!isFormEdit && !clientSecret && (
+          <button className='bg-blue-800' onClick={() => setIsFormEdit(true)}>
             EDIT{' '}
           </button>
         )}
@@ -188,69 +286,18 @@ export default function Recap() {
         {isFormEdit && (
           <form onSubmit={handleSubmit(handleEditForm)}>
             <h1>EDIT SHIPPING INFO</h1>
-            <div className="first-name-field">
-              <label htmlFor="first-name">First name</label>
-              <input id="first-name" type="text" {...register('firstName')} />
+            <div className='first-name-field'>
+              <label htmlFor='first-name'>First name</label>
+              <input id='first-name' type='text' {...register('firstName')} />
             </div>
-            <div className="address-1-field">
-              <label htmlFor="last-name">Last name</label>
-              <input id="last-name" type="text" {...register('lastName')} />
+            <div className='address-1-field'>
+              <label htmlFor='last-name'>Last name</label>
+              <input id='last-name' type='text' {...register('lastName')} />
             </div>
-            <div className="address-1-field">
-              <label htmlFor="email">Email</label>
-              <input id="email" type="text" {...register('email')} />
+            <div className='address-1-field'>
+              <label htmlFor='email'>Email</label>
+              <input id='email' type='text' {...register('email')} />
             </div>
-            <div className="address-1-field">
-              <label htmlFor="address_1">Address_1</label>
-              <input id="address_1" type="text" {...register('address_1')} />
-            </div>
-
-            <div className="address-2-field">
-              <label htmlFor="address_2">Address_2</label>
-              <input id="address_2" type="text" {...register('address_2')} />
-              {errors.address_2 && <p>{errors.address_2.message}</p>}
-            </div>
-
-            <div className="city-field">
-              <label htmlFor="city">City</label>
-              <input id="city" type="text" {...register('city')} />
-            </div>
-
-            <div className="state-field">
-              <label htmlFor="state">State</label>
-              <input id="state" type="text" {...register('state')} />
-            </div>
-
-            <div className="zip-field">
-              <label htmlFor="zip">Zip</label>
-              <input id="zip" type="text" {...register('zip')} />
-            </div>
-
-            <button className="bg-green-800">SAVE CHANGES</button>
-
-            {addressValidationFailed && <h2>INVALID ADDRESS - RE-ENTER</h2>}
-          </form>
-        )}
-      </section>
-      {!isFormEdit && (
-        <div>
-          {clientSecret ? (
-            <Elements stripe={stripePromise} options={options}>
-              <Checkout />
-            </Elements>
-          ) : (
-            <button className="bg-amber-400" onClick={(e) => handleCheckout(e)}>
-              PROCEED TO PAYMENT
-            </button>
-          )}
-        </div>
-      )}
-    </div>
-  );
-}
-
-{
-  /* <form>
             <div className='address-1-field'>
               <label htmlFor='address_1'>Address_1</label>
               <input id='address_1' type='text' {...register('address_1')} />
@@ -277,26 +324,45 @@ export default function Recap() {
               <input id='zip' type='text' {...register('zip')} />
             </div>
 
-            {clientSecret && (
-              <Elements stripe={stripePromise} options={options}>
-                <Checkout options={options} />
-              </Elements>
-            )}
-            <button className='bg-green-800'>EDIT</button>
+            <button className='bg-green-800'>SAVE CHANGES</button>
 
-            <button className='bg-amber-400' onClick={(e) => handleCheckout(e)}>PROCEED TO PAYMENT</button>
+            {addressValidationFailed && <h2>INVALID ADDRESS - RE-ENTER</h2>}
+          </form>
+        )}
+      </section>
+      <section className='promo-section'>
+        <form onSubmit={(e) => handlePromoSubmit(e)}>
+          <label htmlFor='promo-code'>enter your promo-code:</label>
 
-            {/*           
-          <button
-            className={saveIsDisabled ? 'bg-red-500' : 'bg-green-600'}
-            type='submit'
-            disabled={saveIsDisabled}
-          >
-            SAVE
-          </button> */
-}
-
-// {addressValidationFailed && <h2>INVALID ADDRESS - RE-ENTER</h2>}
-{
-  /*</form> */
+          <input
+            id='promo-code'
+            type='text'
+            value={promo}
+            placeholder={promoErrors.status ? 'invalid promo-code' : ''}
+            onChange={(e) => setPromo(e.target.value)}
+          ></input>
+          <button>verify</button>
+        </form>
+      </section>
+      {!isFormEdit && (
+        <div>
+          {clientSecret ? (
+            <Elements stripe={stripePromise} options={options}>
+              <Checkout />
+            </Elements>
+          ) : (
+            <button className='bg-amber-400' onClick={(e) => handleCheckout(e)}>
+              PROCEED TO PAYMENT
+            </button>
+          )}
+        </div>
+      )}
+      {clientSecret && (
+        <button className='bg-red-400' onClick={handleCancel}>
+          CANCEL
+        </button>)
+      }
+    {isCheckoutCancel && <p>checkout cancelled</p>}
+    </div>
+  );
 }
