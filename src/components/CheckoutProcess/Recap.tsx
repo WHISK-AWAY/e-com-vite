6e7a6d89e3e94637632734da9fb0852071381de0
ruralFluffyDill <stacylukavsky@gmail.com>
2023-06-02 22:05:35 -0400

    restructure checkout flow

--- src/components/CheckoutProcess/Recap.tsx
+++ src/components/CheckoutProcess/Recap.tsx
@@ -3,12 +3,13 @@ import { useEffect, useState } from 'react';
 import { selectAuthUserId } from '../../redux/slices/authSlice';
 import {
   addShippingAddress,
+  editShippingAddress,
   editUserAccountInfo,
   fetchSingleUser,
   selectSingleUser,
 } from '../../redux/slices/userSlice';
 import { useForm, useFormState } from 'react-hook-form';
-import { z } from 'zod';
+import { string, z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import axios from 'axios';
 import { StripeElementsOptions, loadStripe } from '@stripe/stripe-js';
@@ -20,23 +21,8 @@ import {
   selectPromo,
   selectPromoErrors,
 } from '../../redux/slices/promoCodeSlice';
-// import { checkAddress } from '../../utilities/googleAddressValidation';
-
-export type ShippingInfoFields = {
-  isDefault: boolean;
-  shipToAddress: {
-    firstName: string;
-    lastName: string;
-    email: string;
-    address_1: string;
-    address_2: string;
-    city: string;
-    state: string;
-    zip: string;
-  };
-};
-
-type EditFormModes = 'edit' | 'new';
+import { TShippingAddress } from '../../redux/slices/userSlice';
+import ManageShippingAddress from '../UserAccount/shippingAddress/ManageShippingAddress';
 
 export default function Recap() {
   const dispatch = useAppDispatch();
@@ -47,14 +33,15 @@ export default function Recap() {
   const [addressValidationFailed, setAddressValidationFailed] = useState(false);
   const [saveIsDisabled, setSaveIsDisabled] = useState(true);
   const [promo, setPromo] = useState<string>('');
+  const [manageShippingAddress, setManageShippingAddress] =
+    useState<boolean>(false);
   const [isCheckoutCancel, setIsCheckoutCancel] = useState<boolean>(false);
   const [clientSecret, setClientSecret] = useState<string>('');
   const [isFormEdit, setIsFormEdit] = useState<boolean>(false);
-  const [addressFormMode, setAddressFormMode] = useState<EditFormModes>('edit');
   const VITE_STRIPE_PUBLIC_KEY = import.meta.env.VITE_STRIPE_PUBLIC_KEY;
   const stripePromise = loadStripe(VITE_STRIPE_PUBLIC_KEY);
 
-  function getDefaultAddress() {
+  function getCurrentAddress() {
     if (user._id) {
       if (user.shippingAddresses.length > 0) {
         return (
@@ -67,11 +54,8 @@ export default function Recap() {
     } else return null;
   }
 
-  const defaultShippingAddress = getDefaultAddress();
+  const currentShippingAddress = getCurrentAddress();
 
-  useEffect(() => {
-    console.log('defaultShippingAddress', defaultShippingAddress);
-  }, [defaultShippingAddress]);
 
   useEffect(() => {
     if (userId) dispatch(fetchSingleUser(userId));
@@ -85,10 +69,6 @@ export default function Recap() {
     e.preventDefault();
     if (promo) dispatch(fetchSinglePromo(promo));
   };
-
-  // useEffect(() => {
-  //   if (promoName) dispatch(fetchSinglePromo(promoName));
-  // }, [promoName]);
 
   const orderDetails = () => {
     console.log('hello from orderDetails function @ Success.tsx');
@@ -139,12 +119,6 @@ export default function Recap() {
     return userOrder;
   };
 
-  // useEffect(() => {
-  //   if (user?._id && userId) {
-  //     dispatch(createOrder({ userId, order: orderDetails() as TOrder }));
-  //   }
-  // }, [user, userId]);
-
   //address shit
 
   useEffect(() => {
@@ -153,59 +127,6 @@ export default function Recap() {
     }
   }, [user]);
 
-  const ZShippingData = z.object({
-    isDefault: z.boolean(),
-    shipToAddress: z.object({
-      firstName: z.string().min(2),
-      lastName: z.string().min(2),
-      email: z.string().email(),
-      address_1: z.string().min(5),
-      address_2: z.string().optional(),
-      city: z.string().min(1),
-      state: z.string().min(2),
-      zip: z.string().min(5),
-    }),
-  });
-
-  const defaultValues: ShippingInfoFields = {
-    shipToAddress: {
-      firstName:
-        defaultShippingAddress?.shipToAddress.firstName || user.firstName || '',
-      lastName:
-        defaultShippingAddress?.shipToAddress.lastName || user.lastName || '',
-      email: defaultShippingAddress?.shipToAddress.email || user.email || '',
-      address_1:
-        defaultShippingAddress?.shipToAddress.address_1 ||
-        user.address?.address_1 ||
-        '',
-      address_2:
-        defaultShippingAddress?.shipToAddress.address_2 ||
-        user.address?.address_2 ||
-        '',
-      city:
-        defaultShippingAddress?.shipToAddress.city || user.address?.city || '',
-      state:
-        defaultShippingAddress?.shipToAddress.state ||
-        user.address?.state ||
-        '',
-      zip: defaultShippingAddress?.shipToAddress.zip || user.address?.zip || '',
-    },
-    isDefault: defaultShippingAddress?.isDefault || false,
-  };
-
-  const {
-    register,
-    reset,
-    handleSubmit,
-    setError,
-    getValues,
-    setValue,
-    formState: { errors, dirtyFields, defaultValues: currentDefaults },
-  } = useForm<ShippingInfoFields>({
-    resolver: zodResolver(ZShippingData),
-    defaultValues,
-    mode: 'onBlur',
-  });
 
   const handleCheckout = async (e: any) => {
     e.preventDefault();
@@ -248,62 +169,26 @@ export default function Recap() {
     },
   };
 
-  const handleEditForm = (data: ShippingInfoFields) => {
-    const userFields = {
-      // TODO: need to restructure this to match ShippingSchema
-      isDefault: data.isDefault,
-      userId: userId!,
-      shipToAddress: data.shipToAddress,
-    };
-
-    if (addressFormMode === 'new') {
-      // dispatch new address thunk
-      dispatch(addShippingAddress({ shippingData: userFields }));
-    } else if (addressFormMode === 'edit') {
-      // dispatch edit address thunk (w/ address ID)
-    }
-    // if (userId) dispatch(editUserAccountInfo({ userId, user: userFields })); // TODO: might point this @ a different thunk
-
-    setIsFormEdit(false);
-  };
 
   // ! Decline card: 4000 0000 0000 9995
 
   const handleCancel = () => {
     setClientSecret('');
     setIsCheckoutCancel(true);
-    setAddressFormMode('edit');
   };
 
-  function newShippingAddress() {
-    setIsFormEdit(true);
-    setAddressFormMode('new');
 
-    reset({
-      shipToAddress: {
-        firstName: '',
-        lastName: '',
-        email: '',
-        address_1: '',
-        address_2: '',
-        city: '',
-        state: '',
-        zip: '',
-      },
-    });
-  }
-
-  function cancelAddressForm() {
-    setIsFormEdit(false);
-
-    reset(defaultValues);
-  }
+  const handleManageShippingAddress = () => {
+    setManageShippingAddress(true);
+  };
 
   if (!user?.cart?.products) return <h1>Loading cart...</h1>;
 
   return (
     <div>
-      <section className="order-recap">
+      <section className='order-recap'>
+
+        
         {/* PRODUCTS RECAP */}
 
         <h1>ORDER RECAP</h1>
@@ -324,161 +209,49 @@ export default function Recap() {
             <p>total: {user.cart.subtotal * (1 - verifyPromo.promoRate)}</p>
           </>
         )}
-        {/* SHIPPING FORM (PLAIN TEXT) */}
-        {user && defaultShippingAddress && !isFormEdit && (
-          <section className="shipping-info-container">
-            <br />
-            <h1>SHIPPING INFO</h1>
-            <div>
-              {defaultShippingAddress.isDefault && <p>(default address)</p>}
-              <p>
-                First name: {defaultShippingAddress.shipToAddress.firstName}
-              </p>
-              <p>Last name: {defaultShippingAddress.shipToAddress.lastName}</p>
-              <p>Email: {defaultShippingAddress.shipToAddress.email}</p>
-              <p>Address 1: {defaultShippingAddress.shipToAddress.address_1}</p>
-              {defaultShippingAddress.shipToAddress.address_2 && (
-                <p>
-                  Address 2: {defaultShippingAddress.shipToAddress.address_2}
-                </p>
-              )}
-              <p>City: {defaultShippingAddress.shipToAddress.city}</p>
-              <p>State: {defaultShippingAddress.shipToAddress.state}</p>
-              <p>Zip: {defaultShippingAddress.shipToAddress.zip}</p>
-            </div>
-          </section>
-        )}
-        {!isFormEdit && !clientSecret && (
-          <>
-            <button className="bg-blue-800" onClick={() => setIsFormEdit(true)}>
-              EDIT{' '}
-            </button>
-            <br />
-            <button className="bg-blue-800" onClick={newShippingAddress}>
-              NEW ADDRESS
-            </button>
-          </>
-        )}
 
-        {/* EDIT SHIPPING INFO FORM */}
-        {isFormEdit && (
-          <>
-            <form onSubmit={handleSubmit(handleEditForm)}>
-              <h1>EDIT SHIPPING INFO</h1>
-              <div className="first-name-field">
-                <label htmlFor="first-name">First name</label>
-                <input
-                  id="first-name"
-                  type="text"
-                  {...register('shipToAddress.firstName')}
-                />
-              </div>
-              <div className="address-1-field">
-                <label htmlFor="last-name">Last name</label>
-                <input
-                  id="last-name"
-                  type="text"
-                  {...register('shipToAddress.lastName')}
-                />
-              </div>
-              <div className="address-1-field">
-                <label htmlFor="email">Email</label>
-                <input
-                  id="email"
-                  type="text"
-                  {...register('shipToAddress.email')}
-                />
-              </div>
-              <div className="address-1-field">
-                <label htmlFor="address_1">Address_1</label>
-                <input
-                  id="address_1"
-                  type="text"
-                  {...register('shipToAddress.address_1')}
-                />
-              </div>
+        {/* PROMO CODE SECTION */}
+        <section className='promo-section'>
+          <form onSubmit={(e) => handlePromoSubmit(e)}>
+            <label htmlFor='promo-code'>enter your promo-code:</label>
 
-              <div className="address-2-field">
-                <label htmlFor="address_2">Address_2</label>
-                <input
-                  id="address_2"
-                  type="text"
-                  {...register('shipToAddress.address_2')}
-                />
-                {errors.shipToAddress?.address_2 && (
-                  <p>{errors.shipToAddress.address_2.message}</p>
-                )}
-              </div>
+            <input
+              id='promo-code'
+              type='text'
+              value={promo}
+              placeholder={promoErrors.status ? 'invalid promo-code' : ''}
+              onChange={(e) => setPromo(e.target.value)}
+            ></input>
+            <button>verify</button>
+          </form>
+        </section>
+      </section>
 
-              <div className="city-field">
-                <label htmlFor="city">City</label>
-                <input
-                  id="city"
-                  type="text"
-                  {...register('shipToAddress.city')}
-                />
-              </div>
-
-              <div className="state-field">
-                <label htmlFor="state">State</label>
-                <input
-                  id="state"
-                  type="text"
-                  {...register('shipToAddress.state')}
-                />
-              </div>
-
-              <div className="zip-field">
-                <label htmlFor="zip">Zip</label>
-                <input
-                  id="zip"
-                  type="text"
-                  {...register('shipToAddress.zip')}
-                />
-              </div>
-
-              <div className="default-field">
-                <label htmlFor="isDefault">
-                  Make this address the default:
-                </label>
-                <input
-                  type="checkbox"
-                  id="isDefault"
-                  defaultChecked={currentDefaults?.isDefault || false}
-                  {...register('isDefault')}
-                />
-              </div>
-
-              <button type="submit" className="bg-green-800">
-                SAVE CHANGES
-              </button>
-              <button
-                type="button"
-                className="bg-red-800"
-                onClick={cancelAddressForm}
-              >
-                CANCEL
-              </button>
-
-              {addressValidationFailed && <h2>INVALID ADDRESS - RE-ENTER</h2>}
-            </form>
-          </>
+      {/* PLAIN TEXT USER ADDRESS */}
+      <section>
+        <br />
+        <h1>SHIPPING DETAILS</h1>
+        <p>first name: {currentShippingAddress?.shipToAddress.firstName}</p>
+        <p>last name: {currentShippingAddress?.shipToAddress.lastName}</p>
+        <p>email: {currentShippingAddress?.shipToAddress.email}</p>
+        <p>address 1: {currentShippingAddress?.shipToAddress.address_1}</p>
+        <p>address 2: {currentShippingAddress?.shipToAddress.address_2}</p>
+        <p>city: {currentShippingAddress?.shipToAddress.city}</p>
+        <p>state: {currentShippingAddress?.shipToAddress.state}</p>
+        <p>zip: {currentShippingAddress?.shipToAddress.zip}</p>
+        <button className='bg-green-300' onClick={handleManageShippingAddress}>
+          MANAGE ADDRESSES
+        </button>
+        {manageShippingAddress && (
+          <ManageShippingAddress
+            user={user}
+            setManageShippingAddress={setManageShippingAddress}
+            currentShippingAddress={currentShippingAddress}
+          />
         )}
       </section>
-      <section className="promo-section">
-        <form onSubmit={(e) => handlePromoSubmit(e)}>
-          <label htmlFor="promo-code">enter your promo-code:</label>
 
-          <input
-            id="promo-code"
-            type="text"
-            value={promo}
-            placeholder={promoErrors.status ? 'invalid promo-code' : ''}
-            onChange={(e) => setPromo(e.target.value)}
-          ></input>
-          <button>verify</button>
-        </form>
-      </section>
+      {/* STRIPE PAYMENT SECTION */}
       {!isFormEdit && (
         <div>
           {clientSecret ? (
@@ -486,14 +259,14 @@ export default function Recap() {
               <Checkout />
             </Elements>
           ) : (
-            <button className="bg-amber-400" onClick={(e) => handleCheckout(e)}>
+            <button className='bg-amber-400' onClick={(e) => handleCheckout(e)}>
               PROCEED TO PAYMENT
             </button>
           )}
         </div>
       )}
       {clientSecret && (
-        <button className="bg-red-400" onClick={handleCancel}>
+        <button className='bg-red-400' onClick={handleCancel}>
           CANCEL
         </button>
       )}
@@ -501,3 +274,4 @@ export default function Recap() {
     </div>
   );
 }
+
