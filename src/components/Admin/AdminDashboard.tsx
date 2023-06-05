cabc35ab5556ed4d9fd6a66e9557fb55e26f0af2
ruralFluffyDill <stacylukavsky@gmail.com>
2023-06-05 13:40:54 -0400

    add admin dashboard component

---
+++ src/components/Admin/AdminDashboard.tsx
@@ -0,0 +1,23 @@
+export default function AdminDashboard() {
+  return (
+    <section className='admin-dashboard'>
+      <h1>ADMIN DASHBOARD</h1>
+      <br/>
+      <section className='inventory-section'>
+        <h1>INVENTORY</h1>
+      </section>
+      <section className='tag-section'>
+        <h1>TAGS</h1>
+      </section>
+      <section className='user-section'>
+        <h1>USERS</h1>
+      </section>
+      <section className='review-section'>
+        <h1>REVIEWS</h1>
+      </section>
+      <section className='reporting-section'>
+        <h1>REPORTS</h1>
+      </section>
+    </section>
+  );
+}
