1f90c3c987bc775620ceb57b6826b3cc83571113
ruralFluffyDill <stacylukavsky@gmail.com>
2023-06-06 20:53:23 -0400

    add links to tags&prods

--- src/components/Admin/AdminDashboard.tsx
+++ src/components/Admin/AdminDashboard.tsx
@@ -1,6 +1,5 @@
-import Inventory from './products/Inventory';
 import { Link } from 'react-router-dom';
-import CreateOrEditProduct from './products/CreateOrEditProduct';
+
 
 export default function AdminDashboard() {
   return (
@@ -8,16 +7,22 @@ export default function AdminDashboard() {
       <h1>ADMIN DASHBOARD</h1>
       <br />
       <section className='inventory-section'>
-        <Link to={'/admin/inventory'} >
+        <Link to={'/admin/inventory'} className='text-blue-300'>
           INVENTORY
-       
         </Link>
-        <br/>
-        <Link to={'/admin/product/new'}>CREATE NEW PRODUCT</Link>
+        <br />
+        <Link to={'/admin/product/new'} className='text-blue-600'>
+          CREATE NEW PRODUCT
+        </Link>
         {/* <Link to={'/admin/product/:productId'}>EDIT PRODUCT</Link> */}
       </section>
       <section className='tag-section'>
-        <h1>TAGS</h1>
+        <Link to={'/admin/tags'} className='text-blue-300'>
+          <h1>TAGS</h1>
+          <Link to={'/admin/tags/new'} className='text-blue-600'>
+            CREATE NEW TAG
+          </Link>
+        </Link>
       </section>
       <section className='user-section'>
         <h1>USERS</h1>
