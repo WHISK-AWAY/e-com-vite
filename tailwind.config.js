a011028016d44eb45df427323b56b06e2fc9b29a
ruralFluffyDill <stacylukavsky@gmail.com>
2023-06-10 18:06:17 -0400

    add fonts & weights+ prom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,51 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      chonburi: ['Chonburi', 'cursive'],
+      grotesque: ['Darker Grotesque', 'sans-serif'],
+      hubbali: ['Hubballi', 'cursive'],
+      italiana: ['Italiana', 'serif'],
+      aurora: ['La Belle Aurore', 'cursive'],
+      marcellus: ['Marcellus', 'serif'],
+      meddon: ['Meddon', 'cursive'],
+      monserrat: ['Montserrat Subrayada', 'sans-serif'],
+      notable: ['Notable', 'sans-serif'],
+      yesYouCan: ['Nothing You Could Do', 'cursive'],
+      paytone: ['Paytone One', 'sans-serif'],
+      poiret: ['Poiret One', 'cursive'],
+      yeseva: ['Yeseva One', 'cursive'],
+      federo: ['Federo', 'sans-serif'],
+    },
+    fontWeight: {
+      light: 300,
+      regular: 400,
+      medium: 500,
+      semibold: 600,
+      bold: 700,
+      xbold: 800,
+      xxbold: 900,
+    },
+    extend: {
+      colors: {
+        charcoal: '#4A4A4A',
+      },
+    },
   },
   plugins: [],
 };
+
+
+// font-family: 'Chonburi', cursive;
+// font-family: 'Darker Grotesque', sans-serif;
+// font-family: 'Hubballi', cursive;
+// font-family: 'Italiana', serif;
+// font-family: 'La Belle Aurore', cursive;
+// font-family: 'Marcellus', serif;
+// font-family: 'Meddon', cursive;
+// font-family: 'Montserrat Subrayada', sans-serif;
+// font-family: 'Notable', sans-serif;
+// font-family: 'Nothing You Could Do', cursive;
+// font-family: 'Paytone One', sans-serif;
+// font-family: 'Poiret One', cursive;
+// font-family: 'Yeseva One', cursive;
