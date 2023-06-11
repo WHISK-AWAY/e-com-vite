88839edf1df7ac6033a8d4a6b3120f7b864327b0
Patrick Bryan <pbryan9@gmail.com>
2023-06-10 18:29:19 -0700

    create stars component

---
+++ src/components/Stars.tsx
@@ -0,0 +1,18 @@
+import starFilled from '../assets/icons/star-filled.svg';
+
+export type StarsProps = {
+  score?: number;
+};
+
+export default function Stars({ score = 3 }: StarsProps) {
+  return (
+    <div className='star-section flex self-start pb-8'>
+      <img src={starFilled} className=' h-4' />
+      <img src={starFilled} className='h-4' />
+      <img src={starFilled} className='h-4' />
+      <img src={starFilled} className='h-4' />
+      <img src={starFilled} className='h-4' />
+      <p className='pl-2 font-grotesque'> {score}</p>
+    </div>
+  );
+}
