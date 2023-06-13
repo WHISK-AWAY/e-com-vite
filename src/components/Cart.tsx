d34c10cf2748036cddd1db2b394e439e6cd35ce7
ruralFluffyDill <stacylukavsky@gmail.com>
2023-06-13 19:21:28 -0400

    add cart styling

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -4,11 +4,18 @@ import { Link } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../redux/hooks';
 import { fetchUserCart, selectCart } from '../redux/slices/cartSlice';
 import CartItem from './CartItem';
+import x from '../../src/assets/icons/x.svg';
+import { selectAuthUserId } from '../redux/slices/authSlice';
 
-export default function Cart() {
+export default function Cart({
+  setIsCartHidden,
+}: {
+  setIsCartHidden: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
   const dispatch = useAppDispatch();
   const userCart = useAppSelector(selectCart);
-  const { userId } = useParams();
+  const userId = useAppSelector(selectAuthUserId);
+  console.log('UID from cart', userId);
 
   useEffect(() => {
     if (userId) dispatch(fetchUserCart(userId));
@@ -17,37 +24,58 @@ export default function Cart() {
   if (!userCart.cart.products?.length) return <p>Your cart is empty</p>;
 
   return (
-    <section className="cart-container">
-      <h1>
-        YOUR CART (
-        {userCart.cart.products.reduce((total, product) => {
-          return total + product.qty;
-        }, 0)}
-        )
-      </h1>
+    <section className='cart-container absolute right-0 top-0 z-10 flex h-[100vh] w-[100vw] flex-col bg-[#35403F]/50 '>
+      <div className='flex h-full max-w-[40vw] flex-col self-end bg-white'>
+        <div className='flex flex-col justify-center border border-red-700  pt-5'>
+          <h1 className='flex justify-center pb-3 font-italiana text-2xl '>
+            YOUR CART (
+            {userCart.cart.products.reduce((total, product) => {
+              return total + product.qty;
+            }, 0)}
+            )
+          </h1>
+            <div className=' border-b border-blue-500 w-full'>
+          <img
+            src={x}
+            alt='x-icon'
+            className='absolute right-0 top-6 h-5 w-10'
+            onClick={() => setIsCartHidden((prev) => !prev)}
+          />
+        </div>
 
-      <br />
-      <div>
-        {userCart.cart.products.map(({ product, qty }) => {
-          return (
-            <CartItem
-              product={product}
-              qty={qty}
-              userId={userId!}
-              key={product._id}
-            />
-          );
-        })}
+          <div className='h-full border m-10 border-green-400 '>
+            {userCart.cart.products.map(({ product, qty }) => {
+              return (
+                <CartItem
+                  product={product}
+                  qty={qty}
+                  userId={userId!}
+                  key={product._id}
+                />
+              );
+            })}
+
+          <div>Subtotal:{userCart.cart.subtotal}</div>
+
+          <div className='flex h-full flex-col items-center gap-3'>
+            <Link
+              to={'/checkout'}
+              className='rounded-sm bg-charcoal px-10 py-3  text-center font-italiana text-xl text-white'
+            >
+              PROCEED TO CHECKOUT
+            </Link>
+            {/* <Recap userCart={userCart}/> */}
+
+            <Link
+              to={'/shop-all'}
+              className='w-fit border border-charcoal px-6 py-2 font-italiana '
+            >
+              continue shopping
+            </Link>
+              </div>
+          </div>
+        </div>
       </div>
-
-      <div>Subtotal:{userCart.cart.subtotal}</div>
-
-      <Link to={'/checkout'} className="bg-teal-700">
-        RECAP
-      </Link>
-      {/* <Recap userCart={userCart}/> */}
-      <br />
-      <Link to={'/shop-all'}>back to shopping</Link>
     </section>
   );
 }
