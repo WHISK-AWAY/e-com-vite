17f07e7baaab491f174e5c9f6aac4ff65c846d1c
ruralFluffyDill <stacylukavsky@gmail.com>
2023-06-14 12:48:56 -0400

    cart styling done(i think)

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -21,58 +21,64 @@ export default function Cart({
     if (userId) dispatch(fetchUserCart(userId));
   }, [userId]);
 
-  if (!userCart.cart.products?.length) return <p>Your cart is empty</p>;
+  if (!userCart.cart.products?.length) return <p>Your cart is empty</p>
 
   return (
-    <section className='cart-container absolute right-0 top-0 z-10 flex h-[100vh] w-[100vw] flex-col bg-[#35403F]/50 '>
-      <div className='flex h-full max-w-[40vw] flex-col self-end bg-white'>
-        <div className='flex flex-col justify-center border border-red-700  pt-5'>
-          <h1 className='flex justify-center pb-3 font-italiana text-2xl '>
-            YOUR CART (
-            {userCart.cart.products.reduce((total, product) => {
-              return total + product.qty;
-            }, 0)}
-            )
-          </h1>
-            <div className=' border-b border-blue-500 w-full'>
-          <img
-            src={x}
-            alt='x-icon'
-            className='absolute right-0 top-6 h-5 w-10'
-            onClick={() => setIsCartHidden((prev) => !prev)}
-          />
-        </div>
+    <section className='cart-container h-screen fixed right-0 top-0 z-10  flex w-[100vw] flex-col overflow-hidden bg-[#35403F]/50'>
+      <div className='flex max-w-[40vw] flex-col self-end h-full  bg-white 2xl:max-w-[40vw] '>
+        <div className='flex flex-col  items-center justify-start h-full lg:gap-4 '>
+          <div className='header w-full border-b border-charcoal pt-5'>
+            <h1 className='flex justify-center pb-3 font-italiana text-base lg:text-2xl'>
+              YOUR CART (
+              {userCart.cart.products.reduce((total, product) => {
+                return total + product.qty;
+              }, 0)}
+              )
+            </h1>
 
-          <div className='h-full border m-10 border-green-400 '>
-            {userCart.cart.products.map(({ product, qty }) => {
-              return (
-                <CartItem
-                  product={product}
-                  qty={qty}
-                  userId={userId!}
-                  key={product._id}
-                />
-              );
-            })}
+            <img
+              src={x}
+              alt='x-icon'
+              className='absolute right-0 top-6 h-3 w-10 lg:h-5'
+              onClick={() => setIsCartHidden((prev) => !prev)}
+            />
+          </div>
 
-          <div>Subtotal:{userCart.cart.subtotal}</div>
-
-          <div className='flex h-full flex-col items-center gap-3'>
-            <Link
-              to={'/checkout'}
-              className='rounded-sm bg-charcoal px-10 py-3  text-center font-italiana text-xl text-white'
-            >
-              PROCEED TO CHECKOUT
-            </Link>
-            {/* <Recap userCart={userCart}/> */}
-
-            <Link
-              to={'/shop-all'}
-              className='w-fit border border-charcoal px-6 py-2 font-italiana '
-            >
-              continue shopping
-            </Link>
+          <div className='h-full overflow-hidden w-full p-6  lg:p-10'>
+            <div className='flex overflow-hidden h-full w-full flex-col justify-between border  border-charcoal p-5 lg:p-10 '>
+              <div className='flex flex-col overflow-auto h-full gap-6'>
+                {userCart.cart.products.map(({ product, qty }) => {
+                  return (
+                    <CartItem
+                      product={product}
+                      qty={qty}
+                      userId={userId!}
+                      key={product._id}
+                    />
+                  );
+                })}
               </div>
+
+              <div className='flex flex-col items-center justify-end  gap-3 pt-5 border-t-[.5px] border-charcoal/80'>
+                <div className='font-italiana text-base lg:text-base xl:text-xl '>
+                  subtotal: ${userCart.cart.subtotal}
+                </div>
+                <Link
+                  to={'/checkout'}
+                  className='rounded-sm bg-charcoal px-6  py-3 text-center  font-italiana text-base text-white lg:px-10 xl:px-14 xl:text-xl'
+                >
+                  PROCEED TO CHECKOUT
+                </Link>
+                {/* <Recap userCart={userCart}/> */}
+
+                <Link
+                  to={'/shop-all'}
+                  className='w-fit border border-charcoal px-6 font-italiana text-sm lg:py-1  xl:text-base '
+                >
+                  continue shopping
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
       </div>
