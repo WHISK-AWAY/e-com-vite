17ac58e0afbed94adcd7a4da5b38232e66c833a9
ruralFluffyDill <stacylukavsky@gmail.com>
2023-06-19 21:37:52 -0400

    start footer styling

---
+++ src/components/Footer.tsx
@@ -0,0 +1,18 @@
+export default function Footer() {
+  return (
+    <section className='footer-section flex h-[400px] max-w-screen text-white justify-end bg-[#262626]'>
+      <section className=' our-story w-2/12'>
+        <div>
+          <h2 className='font-italiana text-lg uppercase flex justify-start'>Our story</h2>
+          <p className='font-aurora'>
+             <span className='font-chonburi uppercase'>astoria</span> is celebrated for its female form, colorful prints
+            and flattering shapes. Pieces that bring out the best of you. Each
+            dress has detailing that makes your body shine. The mission is to
+            create clothes that celebrate all different body types -that will
+            let individuality shine. About us
+          </p>
+        </div>
+      </section>
+    </section>
+  );
+}
