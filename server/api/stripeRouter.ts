eb635f6c3082db8372685b215bcf5f403429cf1d
ruralFluffyDill <stacylukavsky@gmail.com>
2023-06-20 20:19:43 -0400

    add guest payment option

--- server/api/stripeRouter.ts
+++ server/api/stripeRouter.ts
@@ -2,12 +2,10 @@
 import { Response, Request, NextFunction } from 'express';
 import express from 'express';
 import { checkAuthenticated } from './authMiddleware';
-import { User } from '../database/index';
+import { IProduct, Product, TProduct, User } from '../database/index';
 import { number } from 'zod';
-const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY
-const stripe = require('stripe')(
-  STRIPE_SECRET_KEY
-);
+const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
+const stripe = require('stripe')(STRIPE_SECRET_KEY);
 // const express = require('express');
 const router = express.Router();
 
@@ -38,7 +36,6 @@ router.post(
 
       const subtotal = (await userLookup?.cart.subtotal) as unknown as number;
       if (!subtotal) return res.status(400).send('bad');
-    
 
       // Create a PaymentIntent with the order amount and currency
       const paymentIntent = await stripe.paymentIntents.create({
@@ -60,6 +57,51 @@ router.post(
   }
 );
 
+router.post('/create-guest-payment-intent', async (req, res, next) => {
+  try {
+    const cart = req.body;
+    const prodDetails = cart.products.reduce(
+      (accum: { [key: string]: number }, p: Partial<TProduct>) => {
+        accum[p.product!!!._id!.toString()] = p.qty!;
+        return accum;
+      },
+      {}
+    );
+
+    const prodId = Object.keys(prodDetails);
+
+    const productLookup = await Product.find({ _id: { $in: prodId } });
+
+    if (!productLookup || productLookup.length === 0) {
+      return res.status(404).json({
+        message: 'Passed product ID does not match the database records',
+      });
+    }
+
+    const subtotal = productLookup.reduce((accum:number, s:IProduct) => {
+
+      return accum + s.price * prodDetails[s._id!.toString()];
+    }, 0)
+
+
+    console.log('sub', subtotal)
+    if (!subtotal) return res.status(400).send('bad');
+
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: Math.round(subtotal * 100),
+      currency: 'usd',
+      automatic_payment_methods: {
+        enabled: true,
+      },
+    });
+
+    res.json({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
 // app.listen(4242, () => console.log('Running on port 5173'));
 
 export default router;
