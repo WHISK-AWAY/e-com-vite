7c4ee36a67a6a10c7de16f8bdc0324aba1a4c5a1
ruralFluffyDill <stacylukavsky@gmail.com>
2023-06-26 12:29:30 -0400

    add sign wrapper component

--- src/components/SignWrapper.tsx
+++ src/components/SignWrapper.tsx
@@ -2,6 +2,7 @@ import SignIn from "./SignIn"
 import SignUp from "./SignUp"
 import { useState } from "react"
 import x from '../assets/icons/x.svg'
+import whiteX from '../assets/icons/whiteX.svg'
 
 
 export type TMode = 'sign-in' | 'sign-up'
@@ -13,17 +14,25 @@ export default function SignWrapper({setIsSignFormHidden} :
 
   return (
     <section className='form-container fixed right-0 top-0 z-40 flex h-[100vh] w-[100vw] flex-col overflow-hidden bg-[#35403F]/50'>
-      <div className='relative flex h-full w-[33vw] flex-col self-end bg-white 2xl:max-w-[20vw]  '>
+      <div className='relative flex h-full w-[35vw] flex-col self-end bg-white 2xl:max-w-[20vw]  '>
         <div
           onClick={() => setIsSignFormHidden(true)}
-          className='absolute  right-5 top-5 z-50 h-10 w-3 bg-red-300'
+          className='absolute  right-5 top-5 z-50 h-10 w-3'
         >
-          <img src={x} alt='x-icon' className=' w-1 lg:w-2' />
+          {mode === 'sign-in' ? (
+            <img src={x} alt='x-icon' className=' w-1 lg:w-2 ' />
+          ) : (
+            <img src={whiteX} alt='x-icon' className=' w-1 lg:w-2 ' />
+          )}
         </div>
         {mode === 'sign-in' ? (
           <SignIn mode={mode} setMode={setMode} />
         ) : (
-          <SignUp mode={mode} setMode={setMode} />
+          <SignUp
+            mode={mode}
+            setMode={setMode}
+            setIsSignFormHidden={setIsSignFormHidden}
+          />
         )}
       </div>
     </section>
