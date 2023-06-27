dc6646af6e4f79e92ad39777f025beaa6881da99
Patrick Bryan <pbryan9@gmail.com>
2023-06-27 14:08:54 -0700

    link order history product entries to single product view

--- src/components/UserAccount/OrderHistoryProductCard.tsx
+++ src/components/UserAccount/OrderHistoryProductCard.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 export type OrderHistoryProductCardProps = {
   product: {
     productId: string;
@@ -15,14 +17,20 @@ export default function OrderHistoryProductCard({
   return (
     <div className='order-history-product-card flex w-full justify-center gap-5'>
       <div className='image-container basis-1/5'>
-        <img
-          className='aspect-[3/4] w-full border border-charcoal object-cover'
-          src={product.imageURL}
-          alt='product image'
-        />
+        <Link to={`/product/${product.productId}`}>
+          <img
+            className='aspect-[3/4] w-full border border-charcoal object-cover'
+            src={product.imageURL}
+            alt='product image'
+          />
+        </Link>
       </div>
       <div className='info-container flex basis-4/5 flex-col items-center justify-center gap-4'>
-        <h4 className='font-semiboldd uppercase'>{product.productName}</h4>
+        <h4 className='font-semiboldd uppercase'>
+          <Link to={`/product/${product.productId}`}>
+            {product.productName}
+          </Link>
+        </h4>
         <div>
           {`$${product.price.toFixed(2)}/ea x ${product.qty}: `}
           <span className='font-semibold'>
