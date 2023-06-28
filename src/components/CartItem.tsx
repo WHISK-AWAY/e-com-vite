d3ac4b41d88b0d64560a56ee826d133f3b01b2c8
ruralFluffyDill <stacylukavsky@gmail.com>
2023-06-27 20:32:25 -0400

    fixing container width

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -46,44 +46,46 @@ export default function CartItem(props: CartProps) {
 
   return (
     <div className='cart-item-container w-9/10'>
-      <div className=' flex  justify-between '>
+      <div className=' flex items-center justify-center '>
         <img
-          className='aspect-[3/4] lg:h-48 h-32 w-4/12 object-cover'
+          className='aspect-[3/4] h-32 w-4/12 object-cover lg:h-48'
           src={
             images.find((image) => image.imageDesc === 'product-front')
               ?.imageURL || images[0].imageURL
           }
         />
-        <div className='relative flex flex-col w-8/12 items-center'>
+        <div className='relative flex w-7/12 flex-col items-center bg-blue-300'>
           <img
             src={x}
             alt='x-icon'
             onClick={() => handleRemove(_id, qty)}
-            className='absolute right-0 top-0 lg:h-3 lg:w-3 h-2 w-3'
+            className='absolute right-0 top-0 h-2 w-3 lg:h-3 lg:w-3'
           />
-       
 
-          <h4 className='top-10 lg:mt-10 mt-5 mb-2 lg:mx-1 text-center font-hubbali  lg:text-md xl:text-xl text-sm uppercase'>
+          <h4 className='top-10 mb-2 mt-5 text-center font-hubbali text-sm uppercase lg:mx-1 lg:mt-10 lg:text-base xl:text-base 2xl:text-xl'>
             {productName}
           </h4>
-          <div className='text-center font-grotesque lg:text-md xl:text-lg  text-sm mb-1'>${price}</div>
-          <div className='flex xl:w-24 lg:w-16 w-14 items-center self-center rounded-full border justify-around  border-charcoal'>
+          <div className='font-grotesquelg:text-md mb-1 text-center text-sm xl:text-base'>
+            ${price}
+          </div>
+          <div className='flex w-14 mt-2 items-center justify-around self-center rounded-full border border-charcoal lg:w-16  xl:w-24'>
             <img
               src={minus}
               alt='minus-icon'
               onClick={handleDecrement}
-              className='xl:h-5 lg:h-3 h-3'
-              />
-            <span className='xl:px-4 lg:px-2  font-grotesque xl:text-lg lg:text-md text-sm'>{count}</span>
+              className='h-3 lg:h-3 xl:h-5'
+            />
+            <span className='lg:text-md font-grotesque  text-sm lg:px-2 xl:px-4 xl:text-lg'>
+              {count}
+            </span>
             <img
               src={plus}
               alt='plus-icon'
               onClick={handleIncrement}
-              className='xl:h-5 lg:h-3 h-3'
-              />
-          
+              className='h-3 lg:h-3 xl:h-5'
+            />
+          </div>
         </div>
-              </div>
       </div>
     </div>
   );
