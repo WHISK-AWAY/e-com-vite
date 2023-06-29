9257eb9b5b85d583e3c9cdf6205fb5ad021e9c89
Patrick Bryan <pbryan9@gmail.com>
2023-06-29 16:47:36 -0700

    remove unused deps; add Featured comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import SignIn from './components/SignIn';
-import SignUp from './components/SignUp';
 import Homepage from './components/Homepage';
 import AllProducts from './components/AllProducts/AllProducts';
 import SingleProduct from './components/singleProduct/SingleProduct';
 import UserProfile from './components/UserAccount/UserProfile';
-import Cart from './components/Cart';
 import Navbar from './components/Navbar';
-import Checkout from './components/CheckoutProcess/Checkout';
-import Favorite from './components/Favorite';
 import Success from './components/CheckoutProcess/stripe/Success';
 import Failure from './components/CheckoutProcess/stripe/Failure';
 import Recap from './components/CheckoutProcess/Recap';
@@ -25,6 +19,7 @@ import TagInventory from './components/Admin/tags/TagInventory';
 import CreateOrEditTag from './components/Admin/tags/CreateOrEditTag';
 import PromoInventory from './components/Admin/promos/PromoInventory';
 import CreateOrEditPromo from './components/Admin/promos/CreateOrEditPromo';
+import Featured from './components/Featured/Featured';
 import Footer from './components/Footer';
 
 function App() {
@@ -33,17 +28,14 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Homepage />} />
-        {/* <Route path='/sign-in' element={<SignIn />} /> */}
-        {/* <Route path='/sign-up' element={<SignUp />} /> */}
         <Route path='/shop-all' element={<AllProducts />} />
         <Route
           path='/shop-all/bestsellers'
           element={<AllProducts sortKey='saleCount' />}
         />
+        <Route path='/featured' element={<Featured />} />
         <Route path='/product/:productId' element={<SingleProduct />} />
         <Route path='/user/:userId' element={<UserProfile />} />
-        {/* <Route path='/user/:userId/cart' element={<Cart />} /> */}
-        {/* <Route path='/user/:userId/favorites' element={<Favorite />} /> */}
         <Route path='/checkout' element={<Recap />} />
         <Route path='/checkout/success' element={<Success />} />
         <Route path='/checkout/failure' element={<Failure />} />
