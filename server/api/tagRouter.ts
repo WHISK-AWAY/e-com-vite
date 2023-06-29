bff3f9418fb53dcfc3e88b3c7062598faeaad16c
Patrick Bryan <pbryan9@gmail.com>
2023-06-28 17:01:03 -0700

    filter unused tags from tag get route

--- server/api/tagRouter.ts
+++ server/api/tagRouter.ts
@@ -1,64 +1,69 @@
 import { Router } from 'express';
-import { Tag } from '../database/index';
+import { ITag, Tag } from '../database/index';
 import z from 'zod';
 import { requireAdmin } from './authMiddleware';
 
 const zodTag = z.object({
-  tagName: z.string()
-})
+  tagName: z.string(),
+});
 
 const router = Router();
 
-router.get('/', async (req, res, next) => {
+router.get('/', async (_, res, next) => {
   try {
-    const tags = await Tag.find({});
-    res.status(200).json(tags);
+    const tags: ITag[] = await Tag.find({}).populate('products');
+    res
+      .status(200)
+      .json(tags.filter((tag) => tag.products && tag.products.length > 0));
   } catch (err) {
     next(err);
   }
 });
 
+router.post('/', requireAdmin, async (req, res, next) => {
+  try {
+    const parsedBody = zodTag.parse(req.body);
+    if (!parsedBody)
+      return res.status(404).json({ message: 'Tag name is required' });
 
+    const tagToCreate = await Tag.create(parsedBody);
 
-router.post('/', requireAdmin,  async(req, res, next) => {
-  try{
-     const parsedBody = zodTag.parse(req.body);
-     if(!parsedBody) return res.status(404).json({message: 'Tag name is required'});
-
-     const tagToCreate = await Tag.create(parsedBody);
-
-     res.status(201).json(tagToCreate);
-  }catch(err) {
+    res.status(201).json(tagToCreate);
+  } catch (err) {
     next(err);
   }
 });
 
-router.put('/:tagId', requireAdmin, async(req, res, next) => {
-  try{
-    const {tagId} = req.params;
+router.put('/:tagId', requireAdmin, async (req, res, next) => {
+  try {
+    const { tagId } = req.params;
     const parsedBody = zodTag.parse(req.body);
 
-    if(!parsedBody) return res.status(404).json({message: 'Tag name is required'});
-    
-    const tagToUpdate = await Tag.findOneAndUpdate({_id: tagId}, parsedBody);
+    if (!parsedBody)
+      return res.status(404).json({ message: 'Tag name is required' });
 
-    res.status(200).json(tagToUpdate)
-  }catch(err) {
+    const tagToUpdate = await Tag.findOneAndUpdate({ _id: tagId }, parsedBody);
+
+    res.status(200).json(tagToUpdate);
+  } catch (err) {
     next(err);
   }
-})
+});
 
-router.delete('/:tagId', requireAdmin, async(req, res, next) => {
-  try{
-    const {tagId} = req.params;
-    const tagToDelete = await Tag.findByIdAndDelete({_id: tagId});
+router.delete('/:tagId', requireAdmin, async (req, res, next) => {
+  try {
+    const { tagId } = req.params;
+    const tagToDelete = await Tag.findByIdAndDelete({ _id: tagId });
 
-    if(!tagToDelete) return res.status(404).json({message: 'Tag you are trying to delete does not exist'});
+    if (!tagToDelete)
+      return res
+        .status(404)
+        .json({ message: 'Tag you are trying to delete does not exist' });
 
     res.sendStatus(204);
-  }catch(err) {
+  } catch (err) {
     next(err);
   }
-})
+});
 
 export default router;
