869196415a14acbba01aae8da9ee7bf1b79c2753
ruralFluffyDill <stacylukavsky@gmail.com>
2023-06-30 17:59:55 -0400

    more progress on styling

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -9,6 +9,9 @@ import rainLeaves from '../assets/vid/homapage/rain-leaves.mp4';
 import bwSeizure from '../assets/vid/homapage/bw-seizure.mp4';
 import rainbowLady from '../assets/bg-img/homepage/rainbow-lady.jpg';
 import beachLady from '../assets/bg-img/homepage/beach-lady.jpg';
+import grapefrutButt from '../assets/bg-img/homepage/grapefruit-butt.jpg';
+import ladyMask from '../assets/bg-img/homepage/lady-mask.jpg';
+import ladyFacewash from '../assets/bg-img/homepage/lady-facewash.jpg';
 
 export default function Homepage() {
   const allProducts = useAppSelector(selectAllProducts);
@@ -21,7 +24,7 @@ export default function Homepage() {
   // console.log('AP', allProducts)
   if (!randomProd) return <p>...loading</p>;
   return (
-    <div className='relative flex h-full w-screen flex-col justify-center  '>
+    <div className='relative flex h-full w-screen flex-col justify-center overflow-hidden '>
       <div className=' relative flex h-[calc(100dvh_-_64px)] w-full justify-center  self-center px-5 lg:px-10'>
         <video
           src={handLotion}
@@ -147,7 +150,7 @@ export default function Homepage() {
               </Link>
             </div>
           </div>
-          <p className='absolute top-0 z-10 flex w-[30%] translate-y-[90%] flex-col text-center font-aurora text-[3vw] text-black '>
+          <p className='absolute top-0 z-10 flex w-[30%] translate-y-[270%] flex-col text-center font-aurora text-[2vw] text-black '>
             heavy moisturizers are ideal for cold climates or during winter when
             <span className='text-white'>
               the air is dryer but they can be too cloying during the heat of
@@ -155,47 +158,174 @@ export default function Homepage() {
             </span>
           </p>
 
-          <div className='  w-[95%]  pt-[2%]'>
+          <div className='  w-[80%]  pt-[2%]'>
             <img
               src={beachLady}
               alt='lady with a big white hat is laying on the beach'
-              className='aspect-[4/3] object-cover w-full'
+              className='aspect-[4/3] w-full object-cover'
             />
           </div>
 
-          <div className=' relative flex h-full  w-[95%] justify-center gap-7 border '>
+          <div className=' relative flex h-full  w-[65%]  justify-center gap-7  '>
             <div className='text-container flex w-[110%] flex-col'>
-              <div className='left-0 z-50 absolute top-0 h-[27%] w-[30%]  rounded-full bg-black/20'>
-                <p className=' right-0 absolute top-0 w-[80%] -translate-x-[9%] translate-y-[170%] text-start font-aurora text-[1.9vw] leading-relaxed text-white'>
+              <div className='absolute  left-0 top-0 h-[70%] w-[25%] -translate-y-[15%] translate-x-[35%] rounded-full bg-black/20'>
+                <p className=' absolute right-0 top-0 w-[80%] -translate-x-[9%] translate-y-[80%] text-start font-aurora text-[1.4vw] leading-relaxed text-white'>
                   heavy moisturizers are ideal for cold climates or during
                   winter when the air is dryer but they can be too cloying
                   during the heat of summer and don't provide adequate
                 </p>
               </div>
             </div>
-            <div className='video-section   h-full  flex  max-h-screen w-full -translate-y-[10%] justify-center '>
+            <div className='video-section flex  h-full  max-h-screen w-full -translate-x-[50%] -translate-y-[15%] justify-center'>
               <video
                 src={bwSeizure}
                 autoPlay={false}
                 muted={true}
                 loop={true}
-                className='aspect-[1/2] w-[40%]   '
+                className='aspect-[1/2] w-[70%]   '
               />
             </div>
             <div className='product-section  absolute right-0 top-0 w-[60%] translate-x-[60%]'>
               {' '}
               <Link to={'/product/' + randomProd!._id} className=' '>
-                <img
+                {/* <img
                   className='aspect-[4/6] w-[40%] object-cover pt-[2%]'
                   src={
                     randomProd!.images.find(
                       (image) => image.imageDesc === 'product-front'
                     )?.imageURL || randomProd!.images[0].imageURL
                   }
-                />
+                /> */}
+                <p className='pt-2 font-hubbali text-[1.2vw]  uppercase text-white'>
+                  {randomProd.productName}
+                </p>
               </Link>
             </div>
           </div>
+
+          <div className='text-1[vw] w-[45%] pb-[7%] text-center font-aurora leading-loose text-white '>
+            <p>
+              heavy moisturizers are ideal for cold climates or during winter
+              when the air is dryer but they can be too cloying during the heat
+              of summer and don't provide adequate
+            </p>
+          </div>
+
+          <div className='mb-[20%] flex w-full flex-col items-center'>
+            <img
+              src={grapefrutButt}
+              alt='lady  wearing nude leotard holding  grapefruit cut in half pressed to her hips'
+              className='z-10 aspect-square w-[30%] object-cover'
+            />
+            <div className=' text-center '>
+              <p className='-translate-y-[40%] pl-7 font-raleway text-[11vw] font-xbold uppercase  leading-none tracking-[2.5rem] text-white '>
+                treat
+              </p>
+              <p className='-translate-y-[50%] font-caslon-display text-[11vw] uppercase leading-none tracking-[.4rem] text-white'>
+                your skin
+              </p>
+              <p className='-translate-y-[110%] font-raleway text-[6vw] font-xbold uppercase leading-none tracking-widest text-white/40'>
+                to
+              </p>
+              {/* <p className='right-1/2 -translate-y-[70%] translate-x-[3%] whitespace-nowrap font-raleway text-[11vw] font-xbold uppercase leading-none tracking-[3.5rem] text-white'>
+                something
+              </p> */}
+              <p className='-translate-y-[65%] font-raleway text-[15vw] font-xxbold uppercase leading-none text-white'>
+                special
+              </p>
+            </div>
+            <Link
+              to='/shop-all'
+              state={{ filterKey: 'body' }}
+              className='relative border border-white bg-transparent px-[6vw] py-[1.1vw] font-raleway text-[1vw] font-light text-white'
+            >
+              shop body
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      <div className='flex w-full flex-col items-center'>
+        <p className=' -translate-y-[50%]  font-yantramanav text-[10vw] font-xxbold uppercase tracking-widest text-charcoal/60 mix-blend-difference'>
+          unleash{' '}
+        </p>
+        <p className='-translate-y-[550%] font-raleway text-[2vw] font-thin leading-none tracking-wide'>
+          the power of
+        </p>
+      </div>
+
+      <div className='flex w-[90%]   -translate-y-[6%] flex-row-reverse '>
+        <div className='flex  flex-row-reverse '>
+          <img src={ladyMask} alt='' className='aspect-[4/6]  w-[50%]' />
+
+          <div className=' relative flex w-[35%]  flex-col justify-between'>
+            <p className=' font-yantramanav text-[6vw] font-semibold uppercase'>
+              <span className='absolute right-0 top-0  translate-x-[10%] translate-y-[90%] tracking-[2rem] text-[#262626]'>
+                hyd
+              </span>
+              <span className='absolute right-0 top-0 translate-x-[105%] translate-y-[90%] tracking-[1rem]  text-white'>
+                ration
+              </span>
+            </p>
+            <Link
+              to={'/product/' + randomProd!._id}
+              className='flex w-full translate-y-[20%] flex-col items-center'
+            >
+              <img
+                className='aspect-[1/2] w-[50%] object-cover '
+                src={
+                  randomProd!.images.find(
+                    (image) => image.imageDesc === 'product-front'
+                  )?.imageURL || randomProd!.images[0].imageURL
+                }
+              />
+              <p className='text-charcol w-[90%] pt-2 text-center font-hubbali  text-[1.5vw] uppercase'>
+                {randomProd.productName}
+              </p>
+            </Link>
+
+            <Link
+              to='/shop-all'
+              state={{ filterKey: 'body' }}
+              className=' w-[60%] -translate-y-[80%] self-center border border-charcoal bg-transparent px-[3vw] py-[1vw] text-center font-raleway text-[1.1vw] font-light'
+            >
+              shop body
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      <div className='relative flex  flex-col items-center justify-center pt-[25%]'>
+        <div className='absolute right-0 top-0  flex w-[60%] -translate-x-[25%] translate-y-[30%] items-center justify-between gap-[10%] '>
+          <p className='w-full text-center font-aurora text-[2vw] text-[#262626]'>
+            heavy moisturizers are ideal for cold climates or during winter when
+            the air is dryer but they can be too cloying during the heat of
+            summer and don't provide adequate
+          </p>
+
+          <Link to={'/product/' + randomProd!._id} className='flex w-full '>
+            <img
+              className='aspect-[8/9] w-full object-cover '
+              src={
+                randomProd!.images.find(
+                  (image) => image.imageDesc === 'product-front'
+                )?.imageURL || randomProd!.images[0].imageURL
+              }
+            />
+          </Link>
+        </div>
+
+        <div className='flex w-full  flex-col '>
+          <img
+            src={ladyFacewash}
+            alt=''
+            className='aspect-[4/6] w-[60%] self-center'
+          />
+          <p className='w-[30%] self-end -translate-y-[600%] text-center font-aurora text-[2vw] text-[#262626]'>
+            heavy moisturizers are ideal for cold climates or during winter when
+            the air is dryer but they can be too cloying during the heat of
+            summer and don't provide adequate
+          </p>
         </div>
       </div>
 
