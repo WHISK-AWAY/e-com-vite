ff8b7fe58c8e892cc69962cc32859fdac25120ad
ruralFluffyDill <stacylukavsky@gmail.com>
2023-07-04 12:10:52 -0400

    worki in progress

--- src/components/navbar/FaceItem.tsx
+++ src/components/navbar/FaceItem.tsx
@@ -1,12 +1,41 @@
+import { selectTagState } from '../../redux/slices/tagSlice';
+import { useAppSelector } from '../../redux/hooks';
 
-export default function FaceItem() {
-  const faceitems = ['moisturizers', 'oils', 'tinted care', 'masks', 'nighttime skincare', 'eye care', 'acne', 'cleansers', 'exfoliators & peelings', 'essences', 'serums', 'lip care', 'creams' ]
+export default function FaceItem({setIsFaceHidden}: {setIsFaceHidden: React.Dispatch<React.SetStateAction<boolean>>}) {
+  const tagState = useAppSelector(selectTagState);
+  const tagList = tagState.tags;
+  const faceitems = [
+    'moisturizers',
+    'oils',
+    'tinted care',
+    'masks',
+    'nighttime skincare',
+    'eye care',
+    'acne',
+    'cleansers',
+    'exfoliators & peelings',
+    'essences',
+    'serums',
+    'lip care',
+    'creams',
+  ];
+
+    const filteredTags = tagList.filter((tag) =>
+      faceitems.includes(tag.tagName)
+    );
   return (
-    <section>
-      <ul>
-        <li></li>
+    <section className='absolute -bottom-[55%] right-0  flex h-screen w-full flex-col '>
+      <ul className='z-20 h-screen w-screen bg-white pl-10 text-[2vw]'>
+        {filteredTags.map((tag) => (
+          <li
+            key={tag._id}
+            className=''
+            onClick={() => setIsFaceHidden((prev) => !prev)}
+          >
+            {tag.tagName}
+          </li>
+        ))}
       </ul>
-      
     </section>
-  )
+  );
 }
