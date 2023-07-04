860804d282ba79842de51c83eea619580d3113b4
ruralFluffyDill <stacylukavsky@gmail.com>
2023-07-03 23:28:09 -0400

    work in progress

--- src/components/navbar/FaceItem.tsx
+++ src/components/navbar/FaceItem.tsx
@@ -1,6 +1,6 @@
 
 export default function FaceItem() {
-  const faceitems = ['moisturizers', 'oils', 'tint']
+  const faceitems = ['moisturizers', 'oils', 'tinted care', 'masks', 'nighttime skincare', 'eye care', 'acne', 'cleansers', 'exfoliators & peelings', 'essences', 'serums', 'lip care', 'creams' ]
   return (
     <section>
       <ul>
