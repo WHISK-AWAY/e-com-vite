7254307635e9e395709e14b7720b96ff55b4bbd1
ruralFluffyDill <stacylukavsky@gmail.com>
2023-07-04 13:29:58 -0400

    add filtered bodyitems

--- src/components/navbar/BodyItem.tsx
+++ src/components/navbar/BodyItem.tsx
@@ -1,14 +1,50 @@
+import { selectTagState } from "../../redux/slices/tagSlice";
+import { useAppSelector } from "../../redux/hooks";
+import { Link } from "react-router-dom";
 
-export default function BodyItem() {
+
+
+export default function BodyItem({
+  setIsBodyHidden,
+  setIsMenuHidden,
+}: {
+  setIsBodyHidden: React.Dispatch<React.SetStateAction<boolean>>;
+  setIsMenuHidden: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
+
+
+  const tagState = useAppSelector(selectTagState);
+  const tagList = tagState.tags;
+  const bodyitems = [
+    'moisturizers',
+    'creams',
+    'spf',
+
+  ];
+
+  const filteredTags = tagList.filter((tag) => bodyitems.includes(tag.tagName));
   return (
-    <section>
+    <section className='absolute -bottom-[65%] right-0  flex h-screen w-full flex-col'>
+      <ul className='z-20 h-screen w-screen bg-white pl-10 text-[2vw]'>
+        {filteredTags.map((tag) => {
+          const name = tag.tagName;
 
-      <ul>
-        <li>
-          
-        </li>
+          return (
+            <Link to='/shop-all' state={{ filterKey: name }} className=''>
+              <li
+                key={tag._id}
+                className=''
+                onClick={() => {
+                  setIsBodyHidden((prev) => !prev);
+                  setIsMenuHidden((prev) => !prev);
+                }}
+              >
+                {name}
+              </li>
+            </Link>
+          );
+        })}
       </ul>
-      
     </section>
-  )
+  );
 }
