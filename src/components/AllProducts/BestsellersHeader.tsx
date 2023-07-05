2be5b010d4ffb28b5f9511fb3115a84700a530ae
ruralFluffyDill <stacylukavsky@gmail.com>
2023-07-05 13:39:30 -0400

    fixed headers issue

--- src/components/AllProducts/BestsellersHeader.tsx
+++ src/components/AllProducts/BestsellersHeader.tsx
@@ -6,7 +6,7 @@ export default function BestsellersHeader({}: BestsellersHeaderProps) {
   return (
     <>
       <section className='mb-40 w-screen '>
-        <div className='video-wrapper  relative flex h-[calc(100vh_-_164px)] w-full items-start min-[1600px]:w-[2500px] '>
+        <div className='video-wrapper  relative flex h-[calc(100vh_-_164px)] w-full items-start 4xl:w-[1500px] 5xl:w-[1900px] 6xl:w-[2600px]'>
           <video
             muted={true}
             autoPlay={true}
