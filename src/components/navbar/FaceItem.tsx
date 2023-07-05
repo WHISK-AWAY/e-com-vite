6f35b1288b722b314a039c55680cb5c698d4dbb8
Patrick Bryan <pbryan9@gmail.com>
2023-07-05 10:40:49 -0700

    updates to face menu

--- src/components/navbar/FaceItem.tsx
+++ src/components/navbar/FaceItem.tsx
@@ -1,70 +1,68 @@
+import { useEffect, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { selectTagState, fetchAllTags } from '../../redux/slices/tagSlice';
 import { useAppSelector, useAppDispatch } from '../../redux/hooks';
-import { Link } from 'react-router-dom';
-import { useEffect } from 'react';
+
+const faceitems = [
+  'moisturizers',
+  'oils',
+  'tinted care',
+  'masks',
+  'nighttime skincare',
+  'eye care',
+  'acne',
+  'cleansers',
+  'exfoliators & peelings',
+  'essences',
+  'serums',
+  'lip care',
+  'creams',
+  'spf',
+];
 
 export default function FaceItem({
   setIsFaceHidden,
-  setIsMenuHidden
+  setIsMenuHidden,
 }: {
   setIsFaceHidden: React.Dispatch<React.SetStateAction<boolean>>;
   setIsMenuHidden: React.Dispatch<React.SetStateAction<boolean>>;
 }) {
-  const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch();
   const tagState = useAppSelector(selectTagState);
   const tagList = tagState.tags;
 
+  const localParent = useRef<HTMLDivElement>(null);
+  const [menuHeight, setMenuHeight] = useState(0);
+
   useEffect(() => {
-    dispatch(fetchAllTags())
-  }, [])
-
-
-  const faceitems = [
-    'moisturizers',
-    'oils',
-    'tinted care',
-    'masks',
-    'nighttime skincare',
-    'eye care',
-    'acne',
-    'cleansers',
-    'exfoliators & peelings',
-    'essences',
-    'serums',
-    'lip care',
-    'creams',
-    'spf'
-  ];
+    dispatch(fetchAllTags());
+  }, []);
 
   const filteredTags = tagList.filter((tag) => faceitems.includes(tag.tagName));
 
-  if (!filteredTags) return <p>...loading</p>
+  if (!filteredTags) return <p>...loading</p>;
   return (
-    <section className='absolute -bottom-[54%] right-0 flex h-screen  w-full flex-col lg:-bottom-[56%] xl:-bottom-[52%] 2xl:-bottom-[50%]   min-[1538px]:-bottom-0'>
-      <ul className='z-20 h-screen w-screen border border-black bg-white py-[2%] pl-10 text-[2vw] lg:leading-none min-[1536px]:bg-red-200 min-[1536px]:text-[1.3vw] '>
-        {filteredTags.map((tag) => {
-          const name = tag.tagName;
-
-          return (
-            <Link
-              to='/shop-all'
-              state={{ filterKey: name }}
-              className=''
-              key={tag._id}
-            >
-              <li
-                className='hover:underline hover:underline-offset-2'
-                onClick={() => {
-                  setIsFaceHidden((prev) => !prev);
-                  setIsMenuHidden((prev) => !prev);
-                }}
-              >
-                {name}
-              </li>
-            </Link>
-          );
-        })}
-      </ul>
+    <section
+      ref={localParent}
+      className='absolute right-0 top-[65%] z-10 flex h-screen w-screen flex-col flex-wrap border border-black bg-white py-[2%] pl-10 text-[2vw] min-[1536px]:text-[1.3vw]'
+    >
+      {filteredTags.map((tag) => {
+        const name = tag.tagName;
+        return (
+          <Link
+            to='/shop-all'
+            state={{ filterKey: name }}
+            className='border border-blue-700 hover:underline hover:underline-offset-2'
+            onClick={() => {
+              setIsFaceHidden((prev) => !prev);
+              setIsMenuHidden((prev) => !prev);
+            }}
+            key={tag._id}
+          >
+            {name}
+          </Link>
+        );
+      })}
     </section>
   );
 }
