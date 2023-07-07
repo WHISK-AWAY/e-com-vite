9852d082b918d20792e6af11a1d54833e5bc0b29
ruralFluffyDill <stacylukavsky@gmail.com>
2023-07-07 17:41:52 -0400

    set lazyload on assets

--- src/components/NewIn/NewIn.tsx
+++ src/components/NewIn/NewIn.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import 'lazysizes'
 
 import roseLady from '../../assets/vid/new in/lady-sniffing-roses.mp4';
 import gelBg from '../../assets/bg-img/new in/red-gray-gel.jpg';
@@ -20,8 +21,8 @@ export default function NewIn() {
           lip tints
         </h1>
         <video
-          src={roseLady}
-          className='relative z-30 h-screen w-11/12 object-cover object-center'
+          data-src={roseLady}
+          className='lazyload relative z-30 h-screen w-11/12 object-cover object-center'
           muted={true}
           autoPlay={true}
           loop={true}
@@ -29,29 +30,33 @@ export default function NewIn() {
       </header>
       <section className='middle-section relative z-10 flex h-full w-screen flex-col items-center gap-[2vw] p-6 pb-[8vw]'>
         <img
-          src={gelBg}
-          className='middle-section-bg absolute right-0 top-0 z-0 h-full w-full object-cover object-top'
+          data-src={gelBg}
+          data-sizes='auto'
+          className='lazyload middle-section-bg absolute right-0 top-0 z-0 h-full w-full object-cover object-top'
           alt='abstract background'
         />
         <div className='middle-section-columns relative flex w-full basis-3/4 gap-6 pt-[18vw]'>
           <div className='product-image-wrapper group absolute right-1/2 top-0 z-10 h-fit w-1/4 translate-x-[50%]'>
             <img
-              src={tintImageURL}
-              className='absolute aspect-[3/4] w-full translate-y-[-30%] object-cover group-hover:invisible'
+              data-src={tintImageURL}
+              data-sizes='auto'
+              className='lazyload absolute aspect-[3/4] w-full translate-y-[-30%] object-cover group-hover:invisible'
               alt='lineup of tinted lip products'
             />
             <video
-              src={tintGif}
+              data-src={tintGif}
+              data-sizes='auto'
               muted={true}
               autoPlay={true}
               loop={true}
-              className='invisible absolute aspect-[3/4] w-full translate-y-[-30%] object-cover group-hover:visible'
+              className='lazyload invisible absolute aspect-[3/4] w-full translate-y-[-30%] object-cover group-hover:visible'
             />
           </div>
           <div className='mid-left relative z-10 min-h-[70vh] basis-1/2'>
             <video
-              className='aspect-[1/2] w-full rounded-sm object-cover object-top'
-              src={redBlobs}
+              className='lazyload aspect-[1/2] w-full rounded-sm object-cover object-top'
+              data-src={redBlobs}
+              data-sizes='auto'
               muted={true}
               autoPlay={true}
               loop={true}
@@ -59,8 +64,9 @@ export default function NewIn() {
           </div>
           <div className='mid-right relative z-10 basis-1/2'>
             <video
-              className='aspect-[1/2] w-full object-cover object-top'
-              src={redLady}
+              className='lazyload aspect-[1/2] w-full object-cover object-top'
+              data-src={redLady}
+              data-sizes='auto'
               muted={true}
               autoPlay={true}
               loop={true}
@@ -102,8 +108,9 @@ export default function NewIn() {
           made with
         </h2>
         <img
-          className='h-full w-full object-cover'
-          src={roseBg}
+          className='lazyload h-full w-full object-cover'
+          data-src={roseBg}
+          data-sizes='auto'
           alt='close-up photo of a red rose'
         />
         <div className='bottom-text absolute top-0 w-3/5 translate-y-[75%] text-white lg:w-1/2 xl:w-5/12 2xl:w-1/3'>
