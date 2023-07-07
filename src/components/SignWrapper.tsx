610d87e31a255128fad2337fb5d7f9cccb258b6f
ruralFluffyDill <stacylukavsky@gmail.com>
2023-07-07 11:39:14 -0400

    add animation on sign wrapper

--- src/components/SignWrapper.tsx
+++ src/components/SignWrapper.tsx
@@ -1,22 +1,84 @@
-import SignIn from "./SignIn"
-import SignUp from "./SignUp"
-import { useState } from "react"
-import x from '../assets/icons/x.svg'
-import whiteX from '../assets/icons/whiteX.svg'
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+import { useEffect, useRef, useState } from 'react';
+import x from '../assets/icons/x.svg';
+import whiteX from '../assets/icons/whiteX.svg';
+import { gsap } from 'gsap';
 
+export type TMode = 'sign-in' | 'sign-up';
 
-export type TMode = 'sign-in' | 'sign-up'
-
-export default function SignWrapper({setIsSignFormHidden} :
-  {setIsSignFormHidden: React.Dispatch<React.SetStateAction<boolean>>}
-) {
+export default function SignWrapper({
+  setIsSignFormHidden,
+}: {
+  setIsSignFormHidden: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
   const [mode, setMode] = useState<TMode>('sign-in');
+  const blurBg = useRef(null);
+  const wrapperRef = useRef(null);
+  const [reverseSlide, setReverseSlide] = useState<gsap.core.Timeline | null>(
+    null
+  );
+
+  useEffect(() => {
+    if (!wrapperRef) return;
+    const ctx = gsap.context(() => {
+      const tl = gsap.timeline({ duration: 0.3 });
+
+      tl.from(blurBg.current, {
+        opacity: 0,
+        duration: 0.3,
+      });
+      const slider = tl.from(
+        wrapperRef.current,
+        {
+          x: '+=100%',
+          duration: 0.8,
+          ease: 'power4.out',
+          onComplete: (self) => {
+            tl.remove(self);
+            tl.to(wrapperRef.current, {
+              x: '+=100%',
+              duration: 1,
+              ease: 'elastic.out',
+            });
+          },
+        },
+        '<'
+      );
+
+      setReverseSlide(tl);
+    });
+
+    return () => {
+      ctx.revert();
+    };
+  }, []);
+
+
+
+    const closeSlider = (e: any) => {
+      if (e.target.id === 'wrapper')
+        reverseSlide?.reverse().then(() => {
+          setIsSignFormHidden(true);
+        });
+    };
+
 
   return (
-    <section className='form-container fixed right-0 top-0 z-40 flex h-[100vh] w-[100vw] flex-col overflow-hidden bg-[#35403F]/50'>
-      <div className='relative flex h-full w-[35vw] flex-col self-end bg-white 2xl:max-w-[23vw]  '>
+    <section
+      ref={blurBg}
+      onClick={closeSlider}
+      id='wrapper'
+      className='wrapper form-container fixed right-0 top-0 z-40 flex h-[100vh] w-[100vw] flex-col overflow-hidden bg-[#35403F]/60 backdrop-blur'
+    >
+      <div
+        ref={wrapperRef}
+        className='relative flex h-full w-[35vw] flex-col self-end bg-white 2xl:max-w-[23vw]  '
+      >
         <div
-          onClick={() => setIsSignFormHidden(true)}
+          onClick={() =>
+            {reverseSlide?.reverse().then(() => setIsSignFormHidden(true))}
+          }
           className='absolute  right-5 top-5 z-50 h-10 w-3'
         >
           {mode === 'sign-in' ? (
