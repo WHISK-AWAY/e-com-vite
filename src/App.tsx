db01648211d717ac6e18a3501bc352ec91d9f5a2
ruralFluffyDill <stacylukavsky@gmail.com>
2023-07-08 13:03:23 -0400

    white space issue

--- src/App.tsx
+++ src/App.tsx
@@ -38,70 +38,66 @@ function App() {
 
 
   return (
-    <LocomotiveScrollProvider 
-    options={
-      {
-        smooth:true
-      }
-    }
-
-    watch={
-      [
-        <AllProducts />,
-        location
-      ]
-    }
-    containerRef={containerRef}>
-
-    <div
-    ref={containerRef}
-    data-scroll-container
-    // id='data-scroll-container'
-    className='data-scroll-container mx-auto min-h-screen text-charcoal'
+    <LocomotiveScrollProvider
+      options={{
+        smooth: true,
+      }}
+      watch={[<AllProducts />, location]}
+      containerRef={containerRef}
     >
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Homepage />} />
-        <Route path='/shop-all' element={<AllProducts/>} />
-        <Route
-          path='/shop-all/bestsellers'
-          element={<AllProducts sortKey='saleCount'/>}
+      <div
+        ref={containerRef}
+        data-scroll-container
+        // id='data-scroll-container'
+        className='data-scroll-container mx-auto min-h-screen text-charcoal'
+      >
+       
+        <Navbar  />
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+          <Route path='/shop-all' element={<AllProducts />} />
+          <Route
+            path='/shop-all/bestsellers'
+            element={<AllProducts sortKey='saleCount' />}
           />
-        <Route path='/featured' element={<Featured />} />
-        <Route path='/new-in' element={<NewIn />} />
-        <Route path='/product/:productId' element={<SingleProduct />} />
-        <Route path='/user/:userId' element={<UserProfile />} />
-        <Route path='/checkout' element={<Recap />} />
-        <Route path='/checkout/success' element={<Success />} />
-        <Route path='/checkout/failure' element={<Failure />} />
-        <Route path='/admin' element={<AdminDashboard />} />
-        <Route path='/admin/reports' element={<AdminReports />} />
-        <Route path='/admin/reviews' element={<AdminReviews />} />
-        <Route path='/admin/users' element={<AdminUsers />} />
-        <Route
-          path='/admin/users/:userId/orders'
-          element={<AdminUserOrderHistory />}
+          <Route path='/featured' element={<Featured />} />
+          <Route path='/new-in' element={<NewIn />} />
+          <Route path='/product/:productId' element={<SingleProduct />} />
+          <Route path='/user/:userId' element={<UserProfile />} />
+          <Route path='/checkout' element={<Recap />} />
+          <Route path='/checkout/success' element={<Success />} />
+          <Route path='/checkout/failure' element={<Failure />} />
+          <Route path='/admin' element={<AdminDashboard />} />
+          <Route path='/admin/reports' element={<AdminReports />} />
+          <Route path='/admin/reviews' element={<AdminReviews />} />
+          <Route path='/admin/users' element={<AdminUsers />} />
+          <Route
+            path='/admin/users/:userId/orders'
+            element={<AdminUserOrderHistory />}
           />
-        <Route
-          path='/admin/users/:userId/order/:orderId/details'
-          element={<AdminOrderDetails />}
-        />
-        <Route path='/admin/inventory' element={<Inventory />} />
-        <Route path='/admin/tags' element={<TagInventory />} />
-        <Route path='/admin/tags/new' element={<CreateOrEditTag />} />
-        <Route path='/admin/tags/:tagId' element={<CreateOrEditTag />} />
-        <Route path='/admin/product/new' element={<CreateOrEditProduct />} />
-        <Route
-          path='/admin/product/:productId'
-          element={<CreateOrEditProduct />}
+          <Route
+            path='/admin/users/:userId/order/:orderId/details'
+            element={<AdminOrderDetails />}
           />
-        <Route path='/admin/promos' element={<PromoInventory />} />
-        <Route path='/admin/promos/new' element={<CreateOrEditPromo />} />
-        <Route path='/admin/promos/:promoId' element={<CreateOrEditPromo />} />
-      </Routes>
-      <Footer />
-    </div>
-          </LocomotiveScrollProvider>
+          <Route path='/admin/inventory' element={<Inventory />} />
+          <Route path='/admin/tags' element={<TagInventory />} />
+          <Route path='/admin/tags/new' element={<CreateOrEditTag />} />
+          <Route path='/admin/tags/:tagId' element={<CreateOrEditTag />} />
+          <Route path='/admin/product/new' element={<CreateOrEditProduct />} />
+          <Route
+            path='/admin/product/:productId'
+            element={<CreateOrEditProduct />}
+          />
+          <Route path='/admin/promos' element={<PromoInventory />} />
+          <Route path='/admin/promos/new' element={<CreateOrEditPromo />} />
+          <Route
+            path='/admin/promos/:promoId'
+            element={<CreateOrEditPromo />}
+          />
+        </Routes>
+        <Footer />
+      </div>
+    </LocomotiveScrollProvider>
   );
 }
 
