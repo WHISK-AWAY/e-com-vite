78db35f33b8b57e97dd375bbaa409ccb04f2ce3c
Patrick Bryan <pbryan9@gmail.com>
2023-07-15 12:26:46 -0700

    correct container build script

--- build-containers.sh
+++ build-containers.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-docker build -t patty-private:e-com-server server
-docker build -t patty-private:e-com-frontend .
+docker build -t pbryan9/patty-private:e-com-server server
+docker build -t pbryan9/patty-private:e-com-frontend .
