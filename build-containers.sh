c15686e6bdb943f41a82e2cadbbe5b49f1f2d9d5
Patrick Bryan <pbryan9@gmail.com>
2023-07-15 08:53:51 -0700

    remove logs

--- build-containers.sh
+++ build-containers.sh
@@ -1,8 +1,4 @@
 #! /bin/sh
 
-echo 'Building docker containers for e-com project.'
-
 docker build -t patty-private:e-com-server server
 docker build -t patty-private:e-com-frontend .
-
-echo 'Done'
