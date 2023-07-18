025b05f94ce0fb8480292876648a133c23f8c1ed
Patrick Bryan <pbryan9@gmail.com>
2023-07-18 11:36:31 -0700

    refactor app to use userRoutes hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { Route, Routes, useLocation } from 'react-router-dom';
+import { useLocation, useRoutes } from 'react-router-dom';
 import Homepage from './components/Homepage';
 import AllProducts from './components/AllProducts/AllProducts';
 import SingleProduct from './components/singleProduct/SingleProduct';
 import UserProfile from './components/UserAccount/UserProfile';
 import Navbar from './components/navbar/Navbar';
 import Success from './components/CheckoutProcess/stripe/Success';
-import Failure from './components/CheckoutProcess/stripe/Failure';
 import Recap from './components/CheckoutProcess/Recap';
 import AdminDashboard from './components/Admin/AdminDashboard';
 import AdminReports from './components/Admin/Reports/AdminReports';
@@ -27,6 +26,7 @@ import '../src/index.css';
 import { AnimatePresence } from 'framer-motion';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/all';
+import React from 'react';
 function App() {
   const location = useLocation();
   const lenis = new Lenis({
@@ -55,6 +55,107 @@ function App() {
   gsap.ticker.lagSmoothing(0);
   requestAnimationFrame(raf);
 
+  const element = useRoutes([
+    {
+      path: '/',
+      element: <Homepage />,
+    },
+    {
+      path: '/shop-all',
+      element: <AllProducts />,
+    },
+    {
+      path: '/shop-all/bestsellers',
+      element: <AllProducts sortKey='saleCount' />,
+    },
+    {
+      path: '/featured',
+      element: <Featured />,
+    },
+    {
+      path: '/new-in',
+      element: <NewIn />,
+    },
+    {
+      path: 'product/:productId',
+      element: <SingleProduct />,
+    },
+    {
+      path: 'user/:userId',
+      element: <UserProfile />,
+    },
+    {
+      path: 'checkout',
+      element: <Recap />,
+    },
+    {
+      path: '/checkout/success',
+      element: <Success />,
+    },
+    {
+      path: '/admin',
+      element: <AdminDashboard />,
+    },
+    {
+      path: '/admin/reports',
+      element: <AdminReports />,
+    },
+    {
+      path: '/admin/reviews',
+      element: <AdminReviews />,
+    },
+    {
+      path: '/admin/users',
+      element: <AdminUsers />,
+    },
+    {
+      path: '/admin/users/:userId/orders',
+      element: <AdminUserOrderHistory />,
+    },
+    {
+      path: '/admin/users/:userId/order/:orderId/details',
+      element: <AdminOrderDetails />,
+    },
+    {
+      path: '/admin/inventory',
+      element: <Inventory />,
+    },
+    {
+      path: '/admin/tags',
+      element: <TagInventory />,
+    },
+    {
+      path: '/admin/tags/new',
+      element: <CreateOrEditTag />,
+    },
+    {
+      path: '/admin/tags/:tagId',
+      element: <CreateOrEditTag />,
+    },
+    {
+      path: '/admin/product/new',
+      element: <CreateOrEditProduct />,
+    },
+    {
+      path: '/admin/product/:productId',
+      element: <CreateOrEditProduct />,
+    },
+    {
+      path: '/admin/promos',
+      element: <PromoInventory />,
+    },
+    {
+      path: '/admin/promos/new',
+      element: <CreateOrEditPromo />,
+    },
+    {
+      path: '/admin/promos/:promoId',
+      element: <CreateOrEditPromo />,
+    },
+  ]);
+
+  if (!element) return <main>Route location not found...</main>;
+
   return (
     <div
       data-lenis-prevent
@@ -63,47 +164,8 @@ function App() {
       {/* <Preloader/> */}
       <Navbar />
       <AnimatePresence mode='wait'>
-
-      <Routes location={location} key={location.pathname}>
-        <Route path='/' element={<Homepage />} />
-        <Route path='/shop-all' element={<AllProducts />} />
-        <Route
-          path='/shop-all/bestsellers'
-          element={<AllProducts sortKey='saleCount' />}
-          />
-        <Route path='/featured' element={<Featured />} />
-        <Route path='/new-in' element={<NewIn />} />
-        <Route path='/product/:productId' element={<SingleProduct />} />
-        <Route path='/user/:userId' element={<UserProfile />} />
-        <Route path='/checkout' element={<Recap />} />
-        <Route path='/checkout/success' element={<Success />} />
-        <Route path='/checkout/failure' element={<Failure />} />
-        <Route path='/admin' element={<AdminDashboard />} />
-        <Route path='/admin/reports' element={<AdminReports />} />
-        <Route path='/admin/reviews' element={<AdminReviews />} />
-        <Route path='/admin/users' element={<AdminUsers />} />
-        <Route
-          path='/admin/users/:userId/orders'
-          element={<AdminUserOrderHistory />}
-          />
-        <Route
-          path='/admin/users/:userId/order/:orderId/details'
-          element={<AdminOrderDetails />}
-          />
-        <Route path='/admin/inventory' element={<Inventory />} />
-        <Route path='/admin/tags' element={<TagInventory />} />
-        <Route path='/admin/tags/new' element={<CreateOrEditTag />} />
-        <Route path='/admin/tags/:tagId' element={<CreateOrEditTag />} />
-        <Route path='/admin/product/new' element={<CreateOrEditProduct />} />
-        <Route
-          path='/admin/product/:productId'
-          element={<CreateOrEditProduct />}
-          />
-        <Route path='/admin/promos' element={<PromoInventory />} />
-        <Route path='/admin/promos/new' element={<CreateOrEditPromo />} />
-        <Route path='/admin/promos/:promoId' element={<CreateOrEditPromo />} />
-      </Routes>
-          </AnimatePresence>
+        {React.cloneElement(element, { key: location.pathname })}
+      </AnimatePresence>
       <Footer />
     </div>
     // </LocomotiveScrollProvider>
