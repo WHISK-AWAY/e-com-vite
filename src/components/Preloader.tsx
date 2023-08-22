eff21f8614a111c86b313419a56c8d4f9a53a1d6
ruralFluffyDill <stacylukavsky@gmail.com>
2023-08-22 17:19:11 -0400

    preloader demo

--- src/components/Preloader.tsx
+++ src/components/Preloader.tsx
@@ -1,125 +1,239 @@
 import { gsap } from 'gsap';
-import { useEffect } from 'react';
+import { useEffect, useLayoutEffect, useRef } from 'react';
 import face1 from '../assets/bg-img/loader/face1.jpg';
 import face2 from '../assets/bg-img/loader/face2.jpg';
+import vidLoader from '../assets/bg-img/loader/vid1.mp4';
 
 export default function Preloader() {
-  useEffect(() => {
-    document.body.style.overflow = 'hidden';
-    return () => {
-      document.body.style.overflow = '';
-    };
-  }, []);
+  // useEffect(() => {
+  //   document.body.style.overflow = 'hidden';
+  //   return () => {
+  //     document.body.style.overflow = '';
+  //   };
+  // }, []);
+  const vidRef = useRef<HTMLVideoElement>(null)
 
   useEffect(() => {
+    if (!document.querySelector('.logo')) return;
     const ctx = gsap.context(() => {
       const tl = gsap.timeline({ delay: 1 });
 
+    //  const pause = tl.paused('.vid', true)
+  // tl.set('vid1, vid2, vid', {
+  //   display: 'none',
+  //   height: 0,
+  //   overflow: 'hidden',
+  //   backgroundColor: 'white'
+  // });
+
+  tl.set('.slogan', {
+    display: 'none',
+    height: 0,
+  })
+
+      tl.set('.vid', {
+        clipPath: 'polygon(0 1%, 100% 0%, 100% 100%, 0% 100%)',
+        scale: 3,
+        ease: 'expo',
+        duration: 3,
+      });
+
+      tl.set('.loader, .loader-wrapper', {
+        height: 0,
+        display: 'none',
+      });
+
+      tl.from('.logo', {
+        duration: 2,
+        overflow: 'hidden',
+        ease: 'power4.inOut',
+        height: 0,
+        opacity: 0,
+        // yPercent: -80
+        // stagger: .5
+      });
+
       tl.to('.loader-wrapper, .loader', {
-        scale: 9,
-        ease: 'power1.inOut',
-        duration: 2.4,
+        duration: 2,
+        delay: 0.2,
+        scale: 3,
+        height: '100vh',
+        ease: 'slow',
+        display: 'block',
+        // stagger: 0.05,
       });
 
       tl.to(
         '.logo',
         {
-          // yPercent: -20,
+          yPercent: -300,
           duration: 1,
           ease: 'power4',
         },
         '<.8'
       );
-      tl.to(
-        '.logo1',
-        {
-          yPercent: 20,
-          duration: 1,
-          ease: 'power4',
-        },
-        '<.8'
-      );
-
-      tl.to(
-        '.loader-wrapper, .loader',
-        {
-          scale: 0,
-          opacity: 0,
-          display: 'none',
-          ease: 'power3.inOut',
-          // delay: 2.1
-        },
-        '<1'
-      );
+      
+      tl.set('.loader-wrapper, .loader', {
+        scale: 0,
+        opacity: 0,
+        display: 'none',
+        ease: 'power3.inOut',
+        
+        delay: 2
+      });
       tl.to(
         '.clipper-left',
         {
-          delay: 0.8,
+          delay: 2.8,
           // clipPath: 'inset(0 100% 0 0)',
           xPercent: -100,
           ease: 'expo',
           duration: 1.5,
           // duration: 2
         },
-        1.5
-      );
-
-      tl.to(
-        '.clipper-right',
+        3.5
+        );
+        
+    
+        tl.to(
+          '.clipper-right',
         {
-          delay: 0.8,
+          delay: 2.8,
           // clipPath: 'inset(0 0 0 100%)',
           xPercent: 100,
           duration: 1.5,
           ease: 'expo',
           // duration:
         },
-        1.5
-      );
+        3.5
+        );
 
-      tl.to('.black-layout', {
-        ease: 'slow',
-        duration: 1,
-      });
+        
+        // tl.play(5)
+        // //  const og = document.querySelector('.vid')
+        // tl.from('.vid', {
+        //   // clipPath: 'polygon(0% 0%, 100% 0%, 100% 25%, 0% 25%, 0% 50%, 100% 50%, 100% 75%, 0% 75%, 0% 100%, 100% 100%, 100% 0%, 75% 0%, 75% 100%, 50% 100%, 50% 0%, 25% 0%, 25% 100%, 0% 100%)',
+        //   // duration: 3, 
+        //   ease: 'slow'
+        //   // clipPath: 'circle(50% at 50% 50%)',
+        // });
+        //  const clone = og?.cloneNode(true);
+        
+        
+
+        // vidRef.current?.eventCallback('onComplete', vidRef.current.play())
+      //   tl.from('.vid', {
+      //   clipPath: 'polygon(25% 0%, 100% 0%, 75% 100%, 0% 100%)',
+      //   // ease: 'slow',
+      //   // duration: 6,
+      //   // xPercent: '+=100'
+      //   // onComplete: () => {
+      //   //   vidRef.current?.restart()
+      //   // }
+      //   // delay: 4,
+      //   // clipPath: 'polygon(0 1%, 100% 0%, 100% 100%, 0% 100%)',
+      //   ease: 'slow',
+      //   // scale: 2,
+      //   duration: 3
+      //   // scale: 2,
+      // });
+
+      // tl.from(
+      //   '.vid',
+      //   {
+      //     clipPath: 'polygon(25% 0%, 100% 0%, 75% 100%, 0% 100%)',
+      //     // x: '+=100',
+      //     duration: 2,
+      //     ease: 'slow',
+      //   },
+        
+      //   );
+  
+        // tl.from('.vid', {
+        //   // scale: 3, 
+        //   duration: 1.2, 
+        //   // ease: 'power4'
+        // })
+        tl.to('.vid', {
+          // xPercent: 50,
+          clipPath: 'polygon(25% 0%, 100% 0%, 75% 100%, 0% 100%)',
+          ease: 'power4.inOut',
+          duration: 5,
+          scale: 1,
+        });
+        tl.to('.vid', {
+          // x: '+=100',
+          // clipPath: 'polygon(25% 0%, 100% 0%, 75% 100%, 0% 100%)',
+        ease: 'expo',
+        duration: 2,
+        xPercent: 80,
+
+      }, )
+
+      tl.to('.slogan', {
+        display: 'block',
+        height: 100,
+        x: -40,
+        duration: 1, 
+        ease: 'expo'
+      })
+
+   
+
+      // tl.to('.white-layout', {
+      //   display: 'block',
+      //   ease: 'power4',
+      //   xPercent: 50,
+      //   duration: 1,
+      //   height: 100
+      //   // opacity: '40%',
+
+      // }, '<+=4')
+
+
+
+
+
+
+
+      tl.to('.vid-layout', {
+        yPercent: -100,
+        // opacity: 0
+        duration: .7, 
+        ease: 'expo.inOut'
+      }, '<+=4')
     });
 
     return () => {
+// vidRef.play()
       ctx.revert();
     };
   }, []);
 
   return (
-    <div className='pre-loader clipper-container relative z-[1000]'>
-      {/* //   <div className='clipper-left inset-clip absolute left-0 top-0 z-10 flex  h-screen w-[50vw]   justify-end overflow-clip  bg-black'>
-    //     <span className='logo translate-y-[30%]  font-notable  text-[10vw] translate-x-[15%] overflow-hidden uppercase text-white'>
-    //       asto
-    //     </span>
-    //   </div>
-    //   <div className='loader-wrapper  absolute right-1/2 top-0 z-50 h-[300px] w-[1px]  bg-white'>
-    //     <div className='loader z-[100000]  h-[10px] w-full bg-white '></div>
-    //   </div>
-    //   <div className='clipper-right   inset-clip absolute right-0 top-0 z-10 flex h-screen w-[50vw]  justify-start overflow-clip  bg-black'>
-    //     <span className='logo translate-y-[30%] -translate-x-[16%] overflow-hidden font-notable mix-blend-luminosity text-[10vw] uppercase text-white'>
-    //       oria
-    //     </span>
-    //   </div> */}
-
-      <div className='black-layout relative h-screen w-screen bg-white'>
-        <div className='face-container flex h-screen w-screen flex-col items-end justify-start overflow-hidden pr-[16%] pt-[5%]'>
-          <img src={face1} className='aspect-[4/6] w-[24vw] object-cover ' />
-          <img
-            src={face2}
-            className='aspect-[4/6] w-[20vw] -translate-x-[10%] -translate-y-[39%] object-cover'
-          />
-          <div className="text-wrapper flex flex-col">
-
-          <p className='slogan-1 font-raleway text-[3vw]  font-light uppercase absolute top-[40%] left-[30%] text-black'>
-            love your skin
-          </p>
-          <p className="slogan-2 text-black text-[7vw] uppercase absolute bottom-[30%] -right-[30%] font-raleway font-light -translate-x-[250%]">enough</p>
-          </div>
-        </div>
+    <div className='pre-loader clipper-container relative z-[100000]'>
+           <div className='clipper-left inset-clip absolute left-0 top-0 z-10 flex  h-screen w-[50vw]   justify-end overflow-clip  bg-black'>
+        <span className='logo absolute bottom-[10%] right-0 z-[100] inline-block h-fit translate-x-[15%] overflow-visible font-notable text-[10vw] uppercase text-white'>
+          asto
+        </span>
       </div>
+      <div className='loader-wrapper  absolute right-1/2 top-0 z-50 h-screen w-[1px] overflow-visible bg-white'>
+        <div className='loader  h-[10px] w-full bg-white '></div>
+      </div>
+      <div className='clipper-right   inset-clip absolute right-0 top-0 z-10 flex h-screen w-[50vw]  justify-start overflow-clip  bg-black'>
+        <span className='logo absolute   bottom-[10%] left-0 inline-block h-fit -translate-x-[16%]  overflow-visible font-notable text-[10vw] uppercase text-white mix-blend-luminosity'>
+          oria
+        </span>
+      </div>     
+
+      <div className='vid-layout h-screen w-screen bg-white flex  justify-center'>
+        <video ref={vidRef} src={`${vidLoader}#t=7`} muted={true} autoPlay={true} className='vid w-96 aspect-[5/6] poly-clip poly-clip  object-cover'></video>
+         {/* <video ref={vidRef} src={`${vidLoader}#t=7`} muted={true} autoPlay={true} className='vid w-96 aspect-[5/6] poly-clip object-cover'></video> */}
+        {/* <video ref={vidRef} src={`${vidLoader}#t=7`} muted={true} autoPlay={true} className='vid w-96 aspect-[5/6] poly-clip object-cover'></video> */} 
+       <p className="slogan flex flex-col justify-start items-start text-[2vw] font-marcellus absolute top-[30%] left-[1/2] 5xl:left-[30%] uppercase">glow gifted by <span className='font-aurora  lowercase self-center  '>nature</span></p>
+
+      </div>
+      {/* <div className='white-layout bg-red-900 w-screen h-screen flex'></div> */}
     </div>
   );
 }
