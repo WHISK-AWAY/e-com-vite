1b9b7ee2ee70b4daed5b624cb44fe9b46050708a
ruralFluffyDill <stacylukavsky@gmail.com>
2023-09-19 16:05:10 -0400

    add screen bkpts, primary-gray-color

--- tailwind.config.js
+++ tailwind.config.js
@@ -51,14 +51,21 @@ export default {
       colors: {
         charcoal: '#4A4A4A',
         'light-brick': '#DA8E61',
+        'primary-gray': '#262626',
       },
       screens: {
+        xxs: '360px',
         xs: '375px',
+        sm: '412px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
         '2xl': '1440px',
         '3xl': '1536px',
         '4xl': '1920px',
         '5xl': '2560px',
         '6xl': '3440px',
+        short: { raw: '(max-height: 768px) and (min-width: 1440px)' },
       },
     },
   },
@@ -66,7 +73,7 @@ export default {
     extend: {
       backgroundColor: ['active', 'hover'],
       borderColor: ['active'],
-      textColor: ['active']
+      textColor: ['active'],
     },
   },
   modules: {
