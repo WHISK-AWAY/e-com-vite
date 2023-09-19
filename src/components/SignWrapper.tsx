5994830fcee3cdb2f44657f0ad913fd04edc9011
ruralFluffyDill <stacylukavsky@gmail.com>
2023-09-19 19:40:33 -0400

    add anim to an x btn, change contaiener width per bkpts

--- src/components/SignWrapper.tsx
+++ src/components/SignWrapper.tsx
@@ -9,12 +9,16 @@ export type TMode = 'sign-in' | 'sign-up';
 
 export default function SignWrapper({
   setIsSignFormHidden,
+  mobileMenu,
 }: {
   setIsSignFormHidden: React.Dispatch<React.SetStateAction<boolean>>;
+  mobileMenu: boolean;
 }) {
   const [mode, setMode] = useState<TMode>('sign-in');
   const blurBg = useRef(null);
   const wrapperRef = useRef(null);
+  const xIconRef = useRef(null);
+
   const [reverseSlide, setReverseSlide] = useState<gsap.core.Timeline | null>(
     null
   );
@@ -28,15 +32,22 @@ export default function SignWrapper({
         opacity: 0,
         duration: 0.3,
       });
-      const slider = tl.from(
-        wrapperRef.current,
-        {
-          x: '+=100%',
-          duration: 0.8,
-          ease: 'power4.out',
-        },
-        '<'
-      );
+      const slider = tl
+        .from(
+          wrapperRef.current,
+          {
+            x: '+=100%',
+            duration: 0.8,
+            ease: 'power4.out',
+          },
+          '<'
+        )
+        gsap.from(xIconRef.current, {
+          opacity: 0,
+          delay:.9,
+          duration: 1.3,
+          ease: 'slow.inOut',
+        });
 
       setReverseSlide(tl);
     });
@@ -68,20 +79,33 @@ export default function SignWrapper({
       ref={blurBg}
       onClick={clickOff}
       id='wrapper'
-      className='wrapper form-container fixed right-0 top-0 flex h-[100vh] w-[100vw] flex-col overflow-hidden bg-[#35403F]/60 backdrop-blur'
+      className='wrapper form-container fixed right-0 top-0 flex h-[100vh] w-[100svw] flex-col overflow-hidden bg-[#35403F]/60 backdrop-blur'
     >
       <div
         ref={wrapperRef}
-        className='relative flex h-full w-[35vw] flex-col self-end bg-white 2xl:max-w-[23vw]  '
+        className={` ${
+          mobileMenu ? 'h-[100svh] w-[100svw]' : 'w-[35vw]'
+        } relative flex h-full  flex-col self-end bg-white 2xl:max-w-[23vw] portrait:md:w-[65svw] `}
       >
         <div
+          ref={xIconRef}
           onClick={closeSlider}
-          className='absolute right-5 top-5 z-50 h-10 w-3 cursor-pointer'
+          className={` ${
+            mobileMenu ? ' w-9 ' : ' w-3'
+          } absolute right-5 top-5 z-50 h-10  cursor-pointer`}
         >
           {mode === 'sign-in' ? (
-            <img src={x} alt='x-icon' className=' w-1 lg:w-2 ' />
+            <img
+              src={x}
+              alt='x-icon'
+              className={`${mobileMenu ? 'w-4' : 'w-1 lg:w-2 portrait:md:w-full'}`}
+            />
           ) : (
-            <img src={whiteX} alt='x-icon' className=' w-1 lg:w-2 ' />
+            <img
+              src={whiteX}
+              alt='x-icon'
+              className={`${mobileMenu ? 'w-4' : 'w-1 lg:w-2'}`}
+            />
           )}
         </div>
         {mode === 'sign-in' ? (
