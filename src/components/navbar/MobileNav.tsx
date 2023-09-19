8174ce9ae7645a98337f18a8781e2ce3132cdbf2
ruralFluffyDill <stacylukavsky@gmail.com>
2023-09-19 16:05:42 -0400

    add mobile nav

---
+++ src/components/navbar/MobileNav.tsx
@@ -0,0 +1,116 @@
+import menuIcon from '../../assets/icons/menu.svg';
+import heartBlanc from '../../assets/icons/heart-blanc.svg';
+import heartFilled from '../../assets/icons/heart-filled.svg';
+import user from '../../assets/icons/user.svg';
+import bag from '../../assets/icons/bag-blanc.svg';
+import searchIcon from '../../assets/icons/search.svg';
+
+import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+import { selectAuth } from '../../redux/slices/authSlice';
+import {
+  fetchSingleUser,
+  selectSingleUser,
+} from '../../redux/slices/userSlice';
+import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+
+import { TCFMode } from './Navbar';
+import SignWrapper from '../SignWrapper';
+
+export type NavbarProps = {
+  setIsSearchHidden: React.Dispatch<React.SetStateAction<boolean>>;
+  setIsCartFavWrapperHidden: React.Dispatch<React.SetStateAction<boolean>>;
+  setMode: React.Dispatch<React.SetStateAction<TCFMode>>;
+  mode: TCFMode;
+  setIsSignFormHidden: React.Dispatch<React.SetStateAction<boolean>>;
+  isSignFormHidden: boolean;
+};
+
+export default function MobileNav({
+  setIsSearchHidden,
+  setIsCartFavWrapperHidden,
+  setMode,
+  mode,
+  setIsSignFormHidden,
+  isSignFormHidden,
+}: NavbarProps) {
+  const dispatch = useAppDispatch();
+  const { userId } = useAppSelector(selectAuth);
+  const singleUserState = useAppSelector(selectSingleUser);
+
+  useEffect(() => {
+    if (userId) dispatch(fetchSingleUser(userId));
+  }, [userId]);
+
+  return (
+    <nav className='mobile-nav-container align-center sticky top-0 z-40 flex h-16 w-[100vw] items-center justify-between bg-pink-200 p-2'>
+      <section className='flex h-full w-fit items-center  gap-6 pl-3'>
+        {/**hamburger menu section */}
+
+        <img src={menuIcon} alt='Menu' className='h-6' />
+
+        {/**logo section */}
+        <Link to={'/'} className='pb-3 font-notable text-[2.5rem] leading-none text-primary-gray'>
+          A
+        </Link>
+      </section>
+
+      {/**user nav section */}
+      <section className='user-navigation flex h-full w-fit items-center justify-center gap-4 pr-3'>
+        <img
+          src={searchIcon}
+          alt='Search'
+          className='h-6'
+          onClick={() => setIsSearchHidden((prev) => !prev)}
+        />
+        <img
+          src={bag}
+          alt='Cart'
+          className='h-6'
+          onClick={() => {
+            setMode('cart');
+            setIsCartFavWrapperHidden(false);
+          }}
+        />
+        <img
+          src={
+            userId && singleUserState.user?.favorites?.length >= 1
+              ? heartFilled
+              : heartBlanc
+          }
+          alt={
+            userId && singleUserState.user?.favorites?.length >= 1
+              ? 'Favorites (Filled Heart)'
+              : 'Favorites (Empty Heart)'
+          }
+          className='h-6'
+          onClick={() => {
+            setMode('fav');
+            setIsCartFavWrapperHidden(false);
+          }}
+        />
+
+        {userId ? (
+          <Link to={`/user/${userId}`}>
+            {' '}
+            <img
+              src={user}
+              alt={userId ? 'User Account' : 'Sign In'}
+              className='h-6'
+            />
+          </Link>
+        ) : (
+          <>
+            <button onClick={() => setIsSignFormHidden((prev) => !prev)}>
+              <img src={user} className='h-6' />
+            </button>
+            {!isSignFormHidden && (
+              <SignWrapper setIsSignFormHidden={setIsSignFormHidden} />
+            )}
+          </>
+        )}
+      </section>
+    </nav>
+  );
+}
