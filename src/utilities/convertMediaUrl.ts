55f89e1ca81eabc34a6a58dea02c343b5c235011
Patrick Bryan <pbryan9@gmail.com>
2023-09-21 15:00:10 -0700

    refactor convertmediaurl for easier debugging

--- src/utilities/convertMediaUrl.ts
+++ src/utilities/convertMediaUrl.ts
@@ -1,11 +1,14 @@
 export default function convertMediaUrl(mediaUrl: string | undefined) {
-  if (!mediaUrl) return '';
+    if (!mediaUrl) return '';
 
-  let [base, extension] = mediaUrl.split('.');
+    let [base, extension] = mediaUrl.split('.');
 
-  if (['jpeg', 'png', 'jpg'].includes(extension)) return base + '.webp';
+    let convertedUrl = '';
 
-  if (['mp4', 'gif'].includes(extension)) return base + '.webm';
+    if (['jpeg', 'png', 'jpg'].includes(extension))
+        convertedUrl = base + '.webp';
 
-  return mediaUrl;
+    if (['mp4', 'gif'].includes(extension)) convertedUrl = base + '.webm';
+
+    return convertedUrl;
 }
