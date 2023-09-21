309ba86c18b25ee9f3413abe62278e4345e33d2e
ruralFluffyDill <stacylukavsky@gmail.com>
2023-09-20 22:46:30 -0400

    pass MM prop, made adj for md+ porptrs

--- src/components/AllProducts/AllProductsHeader.tsx
+++ src/components/AllProducts/AllProductsHeader.tsx
@@ -8,12 +8,14 @@ export type AllProductsHeaderProps = {
   filter: string;
   // allProdsBg: string;
   randomProd: TProduct;
+  mobileMenu: boolean
 };
 
 export default function AllProductsHeader({
   filter,
   // allProdsBg,
   randomProd,
+  mobileMenu
 }: AllProductsHeaderProps) {
   const [randomProdImage, setRandomProdImage] = useState<string | undefined>(
     undefined
@@ -67,16 +69,18 @@ export default function AllProductsHeader({
   console.log(categoryInfo)
   return (
     <>
-      <section className=' relative flex w-1/2 portrait:w-[100vw]'>
+      <section className=' relative flex w-1/2 portrait:w-[100vw] portrait:md:w-1/2'>
         <h1
-          className='absolute right-0 top-6 font-poiret text-6xl uppercase tracking-wide lg:text-8xl 2xl:top-20 2xl:text-9xl portrait:hidden'
+          className={`${
+            mobileMenu ? 'hidden' : ''
+          } absolute right-0 top-6 font-poiret text-6xl uppercase tracking-wide lg:text-8xl 2xl:top-20 2xl:text-9xl `}
           dangerouslySetInnerHTML={{
             __html: categoryInfo?.splitTitle || <span>problem</span>,
           }}
         ></h1>
 
         {/**mobile header */}
-        <h1 className='w-1/6 -rotate-90 self-end whitespace-nowrap pl-7 font-grotesque text-[2rem] font-bold uppercase leading-none text-primary-gray landscape:hidden'>
+        <h1 className='w-1/6 -rotate-90 self-end whitespace-nowrap pl-7 font-grotesque text-[2rem] font-bold uppercase leading-none text-primary-gray portrait:md:hidden landscape:hidden'>
           {categoryInfo?.category === 'all'
             ? `${categoryInfo?.category} products`
             : categoryInfo?.category}
@@ -89,7 +93,7 @@ export default function AllProductsHeader({
           />
           <img
             src={categoryInfo?.image}
-            className='-z-10   portrait:h-[60svh] max-h-[calc(100svh_-_90px)] object-cover'
+            className='-z-10   max-h-[calc(100svh_-_90px)] object-cover portrait:h-[60svh]'
             height='4493'
             width='2996'
             alt=''
@@ -97,14 +101,14 @@ export default function AllProductsHeader({
         </picture>
       </section>
 
-      <section className='random-product flex basis-1/2 flex-col items-center portrait:basis-0'>
-        <div className='mt-32 flex pb-10 pl-9 font-grotesque text-xs md:mt-24  md:pb-5 lg:mt-40 lg:pl-12 lg:text-base xl:mt-44 2xl:mt-56 2xl:pl-16 2xl:text-lg portrait:mt-4 portrait:pl-20 portrait:pr-2 portrait:text-[1.1rem] landscape:short:text-sm '>
+      <section className='random-product flex basis-1/2 flex-col items-center portrait:basis-0 portrait:md:basis-1/2'>
+        <div className='mt-32 flex pb-10 pl-9 font-grotesque text-xs md:mt-24  md:pb-5 lg:mt-40 lg:pl-12 lg:text-base xl:mt-44 2xl:mt-56 2xl:pl-16 2xl:text-lg portrait:mt-4 portrait:pl-20 portrait:pr-2 portrait:text-[1.1rem] portrait:md:mt-32 portrait:md:px-5 portrait:md:text-[1.1rem] landscape:short:text-sm'>
           Discover our most popular formulations for face, body, hands, and
           hair. All our products are vegan, cruelty-free, and made in France
           with only the ingredients essential to their function.
         </div>
         <div
-          className={`relative flex w-3/5 flex-col justify-center lg:w-4/5 portrait:hidden  ${
+          className={`relative flex w-3/5 flex-col justify-center lg:w-4/5 portrait:hidden portrait:md:flex portrait:md:w-4/5  ${
             randomProdGif || backupImage ? 'group' : ''
           }`}
         >
@@ -154,11 +158,11 @@ export default function AllProductsHeader({
             )}
           </Link>
           <Link to={'/product/' + randomProd._id}>
-            <p className='text-md pb-1  pt-7 text-center font-grotesque text-sm uppercase md:pt-5 md:text-xs lg:text-base 3xl:text-lg'>
+            <p className='text-md pb-1  pt-7 text-center font-grotesque text-sm uppercase md:pt-5 md:text-xs lg:text-base 3xl:text-lg portrait:md:text-[1.1rem]'>
               {randomProd!.productName}
             </p>
           </Link>
-          <p className='text-center font-grotesque text-sm lg:text-base  3xl:text-lg'>
+          <p className='text-center font-grotesque text-sm lg:text-base  3xl:text-lg portrait:md:text-[1.1rem]'>
             ${randomProd!.price}
           </p>
         </div>
