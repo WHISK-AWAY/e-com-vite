e1a9ea2c81a1827f9950b2c9fc4954d4b364340d
ruralFluffyDill <stacylukavsky@gmail.com>
2023-09-21 12:34:25 -0400

    add resp styles for portrait

--- src/components/CheckoutProcess/stripe/Success.tsx
+++ src/components/CheckoutProcess/stripe/Success.tsx
@@ -15,7 +15,7 @@ import hands from '../../../assets/bg-img/order-confirmation/hands.jpg';
 import ladyBack from '../../../assets/bg-img/order-confirmation/lady-back.jpg';
 import { resetPromoState } from '../../../redux/slices/promoCodeSlice';
 
-export default function Success() {
+export default function Success({mobileMenu}: {mobileMenu:boolean} ) {
   const dispatch = useAppDispatch();
   const [params, _] = useSearchParams();
   const userId = useAppSelector(selectAuthUserId);
@@ -61,29 +61,33 @@ export default function Success() {
   return (
     <div className='order-confirmation flex flex-col pb-[7%] pt-[3%]'>
       <div className='flex w-[100vw] flex-col items-center justify-center self-center '>
-        <h1 className='flex w-[45%]  justify-center  rounded-sm border-x border-t border-charcoal py-[1%] font-poiret text-lg tracking-wide 2xl:py-[.5%] '>
+        <h1 className='flex w-[45%]  justify-center  rounded-sm border-x border-t border-charcoal py-[1%] font-poiret text-lg tracking-wide 2xl:py-[.5%] portrait:w-[60%] portrait:md:text-[1.4rem]'>
           ORDER CONFIRMATION
         </h1>
-        <div className='flex h-[35vh] w-[70%] flex-col items-center justify-between border border-charcoal xl:w-[55%]'>
+        <div className='flex h-[35vh] w-[70%] flex-col items-center justify-between border border-charcoal xl:w-[55%] portrait:h-[60svh] portrait:w-[95svw]'>
           <div className='flex h-[85%] flex-col items-center'>
-            <p className='py-[1%] font-grotesque'>
+            <p className='py-[1%] font-grotesque portrait:text-[1rem] portrait:md:text-[1.2rem]'>
               sit back and relax, your order is on its way
             </p>
 
-            <div className='flex h-[75%] justify-center gap-2 object-cover'>
-              <img className='' src={hands} alt='' />
+            <div className='flex h-[75%] justify-center gap-2 object-cover portrait:h-[90%] '>
+              <img
+                className={`${mobileMenu ? 'hidden' : ''}`}
+                src={hands}
+                alt=''
+              />
               <img src={towel} alt='' />
-              <img src={ladyBack} alt='' />
+              <img className='portrait:hidden' src={ladyBack} alt='' />
             </div>
           </div>
 
-          <p className=' pb-2 font-grotesque text-sm uppercase '>
+          <p className=' pb-2 font-grotesque text-sm uppercase portrait:text-center portrait:text-[1.2rem] portrait:md:text-[1.4rem]'>
             order confirmation #{userOrder.singleOrder?._id}
           </p>
         </div>
       </div>
-      <div className='total-container mb-4  flex h-[20%] w-[35%] justify-center self-center border-x  border-b border-charcoal py-1'>
-        <div className='flex flex-col items-start py-1 font-poiret text-sm uppercase'>
+      <div className='total-container mb-4  flex h-[20%] w-[35%] justify-center self-center border-x  border-b border-charcoal py-2 portrait:w-[70%]'>
+        <div className='flex flex-col items-start py-1 font-poiret text-sm uppercase portrait:text-[1.2rem]'>
           {singleOrder.promoCode ? (
             <>
               <p className=''>
@@ -115,13 +119,13 @@ export default function Success() {
         </div>
       </div>
 
-      <div className='details-shipping section flex h-full w-[55%] flex-col gap-5 self-center pt-5  lg:max-h-96  lg:flex-row lg:justify-between 2xl:w-[40%]'>
-        <div className='order details min-h-96 flex w-[50%] flex-col overflow-hidden md:w-full'>
-          <h2 className='order-details w-[70%] self-center border-x border-t border-charcoal py-[2%] text-center font-poiret text-lg uppercase '>
+      <div className='details-shipping section flex h-full w-[60%] flex-col gap-5 self-center pt-5  lg:max-h-[450px]  lg:flex-row lg:justify-between 2xl:w-[40%] portrait:w-[95svw] portrait:flex-col'>
+        <div className='order details min-h-96 flex w-[50%] flex-col overflow-hidden md:w-full portrait:w-[95svw]'>
+          <h2 className='order-details w-[70%] self-center border-x border-t border-charcoal py-[2%] text-center font-poiret text-lg uppercase portrait:md:text-[1.4rem]'>
             order details
           </h2>
 
-          <div className='flex h-full w-full flex-col overflow-auto  border border-charcoal p-[5%]'>
+          <div className='flex h-full w-full flex-col overflow-auto  border border-charcoal p-[5%] '>
             {singleOrder.orderDetails.map((item) => (
               <div
                 className='order-detail-item flex h-full w-[80%] items-center justify-between self-center xl:w-[90%]'
@@ -129,12 +133,16 @@ export default function Success() {
               >
                 <div className='flex w-full flex-row-reverse items-center justify-between gap-4'>
                   <div className='flex w-full flex-col items-center text-center '>
-                    <p className='px-[6%] font-grotesque text-xs uppercase'>
+                    <p className='px-[6%] font-grotesque text-xs uppercase portrait:text-[1rem] portrait:md:text-[1.2rem]'>
                       {' '}
                       {item.productName}
                     </p>
-                    <p className='font-grotesque'>{item.qty}</p>
-                    <p className='font-grotesque'>${item.price}</p>
+                    <p className='font-grotesque portrait:text-[1rem] portrait:md:text-[1.2rem]'>
+                      {item.qty}
+                    </p>
+                    <p className='font-grotesque portrait:text-[1rem] portrait:md:text-[1.2rem]'>
+                      ${item.price}
+                    </p>
                   </div>
                   <img
                     src={item.imageURL}
@@ -146,12 +154,12 @@ export default function Success() {
           </div>
         </div>
 
-        <div className='shipping flex  w-[50%] flex-col md:w-full'>
-          <h2 className='w-[70%] self-center border-x border-t border-charcoal py-[2%] text-center font-poiret text-lg uppercase '>
+        <div className='shipping flex  w-[60%] flex-col md:w-full portrait:w-[95svw]'>
+          <h2 className='w-[70%] self-center border-x border-t border-charcoal py-[2%] text-center font-poiret text-lg uppercase portrait:md:text-[1.4rem]'>
             shipping address
           </h2>
 
-          <div className='flex h-full w-full flex-col items-center justify-center self-center border border-charcoal p-[5%] font-grotesque text-sm uppercase leading-9'>
+          <div className='flex h-full w-full flex-col items-center justify-center self-center border border-charcoal p-[5%] font-grotesque text-sm uppercase leading-9 portrait:text-[1rem] portrait:md:text-[1.2rem]'>
             <p>
               {singleOrder.user?.shippingInfo?.firstName}{' '}
               {singleOrder.user?.shippingInfo?.lastName}
