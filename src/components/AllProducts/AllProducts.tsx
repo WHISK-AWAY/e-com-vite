554865b4c4e29fd8285d33a5719731a13b42c32a
Patrick Bryan <pbryan9@gmail.com>
2023-09-21 10:26:55 -0700

    add prettier config file

--- src/components/AllProducts/AllProducts.tsx
+++ src/components/AllProducts/AllProducts.tsx
@@ -254,7 +254,7 @@ export default function AllProducts({
   gsap.ticker.lagSmoothing(0);
   requestAnimationFrame(raf);
 
-  
+
 
   if (!allProducts.products.length) return <p>...Loading</p>;
   if (!tagState.tags.length) return <p>...Tags loading</p>;
@@ -351,13 +351,13 @@ export default function AllProducts({
                 className={` ${allProducts.products.length % 2 === 0
                   ? 'portrait:first-of-type:col-span-full portrait:last-of-type:col-span-full'
                   : 'portrait:[&:nth-of-type(3)]:col-span-full landscape:[&:nth-of-type(5)]:col-span-2 landscape:[&:nth-of-type(5)]:row-span-2 '
-                  } relative flex list-none flex-col justify-between   border-primary-gray landscape:border-b landscape:border-l landscape:last-of-type:border-r landscape:[&:nth-of-type(4)]:border-r [&:nth-of-type(7)]:border-r`}
+                  } relative flex list-none flex-col justify-between border-primary-gray landscape:border-b landscape:border-l landscape:last-of-type:border-r landscape:[&:nth-of-type(4)]:border-r [&:nth-of-type(7)]:border-r`}
                 key={product._id.toString()}
               >
                 <div
-                  className={`aspect-[3/4] h-full transform border-primary-gray transition duration-300 even:border-l-0 hover:scale-105   group-hover:scale-105  group-hover:ease-in-out portrait:aspect-[4/5] portrait:border portrait:odd:border-r-0  ${
-                    hoverURL || hoverFallback ? 'group' : ''
-                  }`}
+                  // hover:scale-105 group-hover:scale-105 
+                  className={`aspect-[3/4] h-full transform border-primary-gray transition duration-300 overflow-hidden even:border-l-0 group-hover:ease-in-out portrait:aspect-[4/5] portrait:border portrait:odd:border-r-0 ${hoverURL || hoverFallback ? 'group' : ''
+                    }`}
                 >
                   <Link
                     to={'/product/' + product._id}
@@ -368,7 +368,8 @@ export default function AllProducts({
                       <img
                         src={imageURL}
                         alt={`product image: ${product.productName}`}
-                        className='h-full w-full object-cover group-hover:invisible '
+                        // group-hover:h-[105%] group-hover:w-[105%] group-hover:invisible 
+                        className='h-[calc(100%_-_2px)] w-[calc(100%_-_2px)] object-cover object-center transition-all duration-300'
                         height='1600'
                         width='1600'
                       />
@@ -381,7 +382,7 @@ export default function AllProducts({
                         muted
                         playsInline
                         controls={false}
-                        className='invisible absolute right-0 top-0 aspect-[3/4] w-full object-cover  group-hover:visible'
+                        className='invisible absolute right-0 top-0 h-[calc(100%_-_2px)] w-[calc(100%_-_2px)] object-cover object-center group-hover:visible group-hover:scale-105 transition-transform duration-300'
                       >
                         <source src={hoverURL} type={hoverURL.split('.').at(-1) === 'mp4' ? 'video/mp4' : 'image/gif'} />
                         <source src={convertMediaUrl(hoverURL)} type='video/webm' />
@@ -395,7 +396,7 @@ export default function AllProducts({
                         <img
                           src={hoverFallback}
                           alt={`alternate image: ${product.productName}`}
-                          className='invisible absolute right-0 top-0 aspect-[3/4] w-full object-cover group-hover:visible'
+                          className='invisible absolute right-0 top-0 h-[calc(100%_-_2px)] w-[calc(100%_-_2px)] object-cover object-center group-hover:visible group-hover:scale-105 transition-transform duration-300'
                           height='1600'
                           width='1600'
                         />
@@ -453,11 +454,10 @@ export default function AllProducts({
 
                 <div className='place-items-stretch border-primary-gray px-2  text-start portrait:border-l portrait:pb-4 landscape:border-t'>
                   <p
-                    className={`${
-                      product.productName.length > 10
+                    className={`${product.productName.length > 10
                         ? 'overflow-hidden text-ellipsis whitespace-nowrap text-xs lg:text-sm 2xl:text-base'
                         : ''
-                    } pt-2   font-grotesque text-xs lg:text-sm 2xl:text-base portrait:pt-1`}
+                      } pt-2   font-grotesque text-xs lg:text-sm 2xl:text-base portrait:pt-1`}
                   >
                     <Link to={'/product/' + product._id}>
                       {product.productName.toUpperCase()}
