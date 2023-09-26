3b9bdec5fc7dd096c072a32a0841c9fedd4459a4
Patrick Bryan <pbryan9@gmail.com>
2023-09-26 09:55:24 -0700

    drop order history columns in portrait layout

--- src/components/UserAccount/OrderHistorySummary.tsx
+++ src/components/UserAccount/OrderHistorySummary.tsx
@@ -20,31 +20,36 @@ export default function OrderHistorySummary({
   setDetailOrder,
 }: OrderHistorySummaryProps) {
   return (
-    <div className='grid-wrapper grid grid-cols-6 place-items-stretch justify-around text-center portrait:w-full portrait:grid-cols-3 portrait:md:text-base'>
+    <div className="grid-wrapper grid grid-cols-6 place-items-stretch justify-around text-center portrait:w-full portrait:grid-cols-4 portrait:md:text-base">
       {columnHeaders.map((col) => (
-        <div key={col} className='mb-1 border-b border-charcoal font-semibold'>
+        <div
+          key={col}
+          className={`mb-1 border-b border-charcoal font-semibold ${
+            ['subtotal', 'discount'].includes(col) ? 'portrait:hidden' : ''
+          }`}
+        >
           {col}
         </div>
       ))}
       {allOrders.map((order) => (
         <React.Fragment key={order._id}>
-          <div className='order-number place-self-center'>
+          <div className="order-number place-self-center">
             {'...' + order._id?.slice(-5)}
           </div>
-          <div className='order-date'>
+          <div className="order-date">
             {new Date(order.date).toLocaleDateString()}
           </div>
-          <div className='order-subtotal place-self-end'>
+          <div className="order-subtotal place-self-end portrait:hidden">
             {order.subtotal!.toFixed(2)}
           </div>
-          <div className='order-discount place-self-end'>
+          <div className="order-discount place-self-end portrait:hidden">
             {(order.total! - order.subtotal!).toFixed(2)}
           </div>
-          <div className='order-total place-self-end'>
+          <div className="order-total place-self-end">
             {order.total!.toFixed(2)}
           </div>
           <button
-            className='place-self-center'
+            className="place-self-center"
             onClick={() => {
               setDetailOrder(order);
             }}
