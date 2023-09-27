23f6498b43d1e154d06dcc2792648245eb14b5d0
ruralFluffyDill <stacylukavsky@gmail.com>
2023-09-27 19:55:47 -0400

    add mobile feature

--- src/components/navbar/CartQtyIndicator.tsx
+++ src/components/navbar/CartQtyIndicator.tsx
@@ -17,7 +17,9 @@ export default function CartQtyIndicator({ setMode, setIsCartFavWrapperHidden, m
             setMode('cart');
             setIsCartFavWrapperHidden(false);
           }}
-          className="absolute right-0 top-3 z-0 flex aspect-square w-[.8rem] -translate-y-1/2 items-center justify-center rounded-full bg-primary-gray font-grotesque text-[.5rem] leading-[0] text-white lg:right-[.5] lg:top-4 lg:w-[1rem] lg:text-[.7rem] 3xl:w-[1.2rem] 3xl:pb-1 3xl:text-[.8rem]"
+          className={` ${
+            mobileMenu ? 'w-[1.4rem] text-[1rem] -right-11' : 'w-[.8rem] text-[.5rem]'
+          } absolute right-0 top-3 z-0 flex aspect-square  -translate-y-1/2 items-center justify-center rounded-full bg-primary-gray font-grotesque  leading-[0] text-white lg:right-[.5] lg:top-4 lg:w-[1rem] lg:text-[.7rem] 3xl:w-[1.2rem] 3xl:pb-1 3xl:text-[.8rem]`}
         >
           {cartItemsQty}
         </div>
