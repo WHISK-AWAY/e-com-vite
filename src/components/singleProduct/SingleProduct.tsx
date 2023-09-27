1c06fdcd8f08e377f9b68e114ad4df3883702f5b
ruralFluffyDill <stacylukavsky@gmail.com>
2023-09-26 23:27:32 -0400

    change btn anim

--- src/components/singleProduct/SingleProduct.tsx
+++ src/components/singleProduct/SingleProduct.tsx
@@ -546,27 +546,14 @@ export default function SingleProduct({
                     </div>
                   )}
                 </div>
-                {/* <button
-                onClick={handleAddToCart}
-                disabled={maxQty === 0}
-                className='mt-14 w-4/5 max-w-[255px] rounded-sm bg-charcoal py-2 font-italiana text-lg  uppercase text-white outline outline-slate-800 hover:outline-offset-4 disabled:bg-charcoal/40 lg:max-w-[400px] lg:text-2xl xl:max-w-[475px] xl:py-3 xl:text-3xl 2xl:py-4 active:border border-red-500 '
-              >
-                add to cart
-              </button> */}
-
+          
                 <button
                   onClick={handleAddToCart}
                   disabled={maxQty === 0}
-                  className="hover:duration-00 group relative mt-[6%] w-4/5 max-w-full overflow-hidden  rounded-sm border-charcoal bg-charcoal py-[2%] font-poiret text-[2vw] font-medium  uppercase text-white  transition-all hover:scale-[1.01] active:bg-red-300 active:ease-in-out disabled:bg-charcoal/40 5xl:text-[1.1vw] portrait:w-full portrait:py-2 portrait:text-[1.6rem] portrait:lg:py-4 portrait:lg:text-[2.3rem]"
-                  aria-label="add to cart"
+                  className="group relative mt-[6%] inline-block w-4/5  max-w-full  font-medium text-white focus:outline-none "
                 >
-                  <span className="ease absolute left-0 top-0 h-0 w-0 border-t-4 border-white transition-all duration-1000  group-hover:w-full "></span>
-                  <span className="ease absolute bottom-0 right-0 h-0 w-0 border-b-4 border-white transition-all duration-500  group-hover:w-full"></span>
-                  <span className="ease absolute left-0 top-0 h-0 w-full bg-gray-400 transition-all  delay-200 duration-1000  group-hover:h-full"></span>
-                  <span className="ease absolute bottom-0 left-0 h-0 w-full bg-gray-400 transition-all delay-200 duration-1000  group-hover:h-full"></span>
-                  <span className="absolute inset-0 h-full w-full border border-charcoal/80 bg-[#383838] opacity-0 delay-500 duration-700 active:bg-yellow-400 group-hover:opacity-100"></span>
-
-                  <span className="ease relative transition-colors delay-200 duration-1000  ">
+                  <span className="absolute inset-0  border group-disabled:border-none border-primary-gray group-active:border-charcoal"></span>
+                  <span className="ease block rounded-sm border border-charcoal bg-charcoal px-12 py-3 font-poiret text-[2vw] font-medium uppercase transition-transform duration-300 active:rounded-sm active:border-charcoal/90 active:bg-charcoal/90 group-disabled:bg-charcoal/40 group-hover:-translate-x-1 group-disabled:-translate-x-0 group-disabled:-translate-y-0 group-hover:-translate-y-1 group-disabled:border-none">
                     add to cart
                   </span>
                 </button>
