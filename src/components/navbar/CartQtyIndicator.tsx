4ab64381dfd887216ed603dfeea61cca9477caf0
ruralFluffyDill <stacylukavsky@gmail.com>
2023-09-28 00:05:00 -0400

    deploy test

--- src/components/navbar/CartQtyIndicator.tsx
+++ src/components/navbar/CartQtyIndicator.tsx
@@ -23,7 +23,7 @@ export default function CartQtyIndicator({
           }}
           className={` ${
             mobileMenu
-              ? '-right-[2.6rem] w-[1.4rem] text-[1rem]'
+              ? '-right-[2.6rem] w-[1.4rem] text-[1rem] pb-1'
               : 'right-0 w-[.8rem] text-[.5rem]'
           } absolute top-3 z-0 flex aspect-square -translate-y-1/2 items-center justify-center rounded-full bg-primary-gray font-grotesque  leading-[0] text-white lg:right-[.5] lg:top-4 lg:w-[1rem] lg:text-[.7rem] 3xl:w-[1.2rem] 3xl:pb-1 3xl:text-[.8rem] portrait:md:text-[.8rem] portrait:md:w-[1.2rem] portrait:md:top-5 portrait:md:right-[.1rem]`}
         >
