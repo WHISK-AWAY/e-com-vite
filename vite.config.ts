6e09d71102b35fb87c8ad7381083991b73c94694
Patrick Bryan <pbryan9@gmail.com>
2023-09-28 14:13:11 -0700

    split off package chunks

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,35 @@
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('react-hook-form')) {
+            return 'hook-form';
+          } else if (id.includes('zod')) {
+            return 'zod';
+          } else if (id.includes('react-query')) {
+            return 'react-query';
+          } else if (id.includes('react-select') || id.includes('emotion')) {
+            return 'react-select';
+          } else if (id.includes('chart')) {
+            return 'chartjs';
+          } else if (id.includes('stripe')) {
+            return 'stripe';
+          } else if (id.includes('framer')) {
+            return 'framer';
+          } else if (id.includes('fuse')) {
+            return 'fuse';
+          } else if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
   publicDir: 'src/public',
 });
